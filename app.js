56e5c2c2e48bb58391ac77311b402c63ea719587
Amos Wong <amoswwj@gmail.com>
2017-02-07 01:24:21 +0800

    Rename express demo users route to api route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
