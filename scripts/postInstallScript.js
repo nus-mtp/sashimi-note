b49600b360c09fe1f0a93dd5046b320b62f1361c
amoshydra <amoswwj@gmail.com>
2017-02-11 16:19:05 +0800

    Reinitialise repo to use webpack
    - Webpack and platform is now in separate folder

---
+++ scripts/postInstallScript.js
@@ -0,0 +1,15 @@
+console.log(`
+
+ +----------------[ Sashimi Note ]----------------+ 
+ |                                                |
+ |  Hi developer!                                 |
+ |                                                |
+ |  You performed npm install in the root folder  |
+ |                                                |
+ |  You should do also do npm install inside      |
+ |  'sashimi-webapp' or 'sashimi-platform'        |
+ |                                                |
+ |                                                |
+ +------------------------------------------------+
+
+`);
