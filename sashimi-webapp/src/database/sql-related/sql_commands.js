beb392dcdc6f669259c3ae4068e35f73d18e23be
Daphne <won.jr@hotmail.com>
2017-02-12 15:49:30 +0800

    Fix eslint problem (alasql) in sql_commands.js
    [1] add alasql dependency
    [2] create a string concatenation support function

--- sashimi-webapp/src/database/sql-related/sql_commands.js
+++ sashimi-webapp/src/database/sql-related/sql_commands.js
@@ -1,5 +1,16 @@
+const alasql = require('alasql');
+
+const stringConcat = function(...stringToConcat) {
+  let fullString = '';
+  for (let index = 0; index < stringToConcat.length; index+=1) {
+    fullString += stringToConcat[index];
+  }
+  return fullString;
+};
+
 const sqlCommands = function() {
   this.get_full_table_data = function(tableName) {
     return alasql(stringConcat('SELECT * FROM ', tableName));
   };
 };
+
