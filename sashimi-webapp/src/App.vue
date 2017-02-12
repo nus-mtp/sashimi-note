a0814efb77caabea54b978476357e58b0ff64f6a
jiayingy <jiayinglek@gmail.com>
2017-02-12 22:58:57 +0800

    Edit codemirror styling

--- sashimi-webapp/src/App.vue
+++ sashimi-webapp/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="container" id="app">
     <router-view></router-view>
   </div>
 </template>
@@ -12,13 +12,25 @@ export default {
 
 <style lang="scss">
 @import "assets/vendors/bootstrap-grid";
+body {
+  margin: 0;
+}
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: black;
+}
+
+.container {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+
+.row {
+  margin: 0;
 }
 
 button {
@@ -28,5 +40,33 @@ button {
 
 button:focus {
   outline: 0;
+}
+
+.CodeMirror {
+  width: 50%; 
+  float: left;
+  border-right: 1px solid black;
+
+  .CodeMirror-gutters {
+    background-color: white;
+    border-right: 1px solid black;
+  }
+}
+
+.CodeMirror-lines {
+  .CodeMirror-code {
+    line-height: 1.8em;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+}
+
+.CodeMirror-linenumber.CodeMirror-gutter-elt {
+  color: black;
+}
+
+.CodeMirror-activeline {
+  .CodeMirror-activeline-background {
+    background-color: #e6e6e6;
+  }
 }
 </style>
