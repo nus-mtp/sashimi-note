db07dbe97a40218de33f0ecbfa38d2365155421b
Daphne <won.jr@hotmail.com>
2017-02-12 15:54:52 +0800

    Add create table function to sqlCommands.js

--- sashimi-webapp/src/database/sql-related/sqlCommands.js
+++ sashimi-webapp/src/database/sql-related/sqlCommands.js
@@ -12,5 +12,9 @@ const sqlCommands = function() {
   this.get_full_table_data = function(tableName) {
     return alasql(stringConcat('SELECT * FROM ', tableName));
   };
+
+  this.create_table = function(sqlStatement) {
+    alasql(sqlStatement);
+  };
 };
 
