f405cee46ab226f2349b11bbed76c2d561224d32
Daphne <won.jr@hotmail.com>
2017-02-12 21:56:44 +0800

    Add initCreateTable function into tableCreator.js

---
+++ sashimi-webapp/src/database/create/tableCreator.js
@@ -0,0 +1,35 @@
+const constants = require('../constants');
+
+const sqlCommands = require('../sql-related/sqlCommands');
+
+const exceptions = require('../exceptions');
+
+let isTableInitializedForCreation = constants.CONST_TABLE_CREATION_CLOSED;
+let sqlCreateTableString = constants.STRING_INITIALIZE;
+
+const stringConcat = function stringConcat(...stringToConcat) {
+  let fullString = '';
+  for (let index = 0; index < stringToConcat.length; index += 1) {
+    fullString += stringToConcat[index];
+  }
+  return fullString;
+};
+
+class tableCreator {
+  static constructor() { }
+
+  static initCreateTable(tableName) {
+    if (isTableInitializedForCreation) {
+      throw new exceptions.TableCreationAlreadyInitiated('Table creation is already initiated. Please close the thread first.');
+    } else {
+      try {
+        sqlCommands.getFullTableData(tableName);
+        throw new exceptions.TableAlreadyExists('Table already Exist. Not available for creation.');
+      } catch (exceptionTableDoesNotExists) {
+        sqlCreateTableString = stringConcat('CREATE TABLE ', tableName, '(');
+        isTableInitializedForCreation = constants.CONST_TABLE_CREATION_INITIALIZED;
+      }
+    }
+  }
+
+module.export = tableCreator;
