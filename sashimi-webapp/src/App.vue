0ef36281bf4a77c1652a92668d784cd27bf6b0fc
jiayingy <jiayinglek@gmail.com>
2017-02-12 20:28:13 +0800

    Import bootstrap-grid.scss file into App.vue

--- sashimi-webapp/src/App.vue
+++ sashimi-webapp/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -11,12 +10,13 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "assets/vendors/bootstrap-grid";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
