591d2832abfce29840c396a24aa1e48a55162ae4
jiayingy <jiayinglek@gmail.com>
2017-02-12 20:23:40 +0800

    Add editor with codemirror with one-way binding, no styling

---
+++ sashimi-webapp/src/components/Editor.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <codemirror v-model="content" @changed="codeChange" > 
+    </codemirror>
+    <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+import { codemirror } from 'vue-codemirror';
+import cm from '../mock/contentManipulator';
+
+/* eslint prefer-const: 0 */
+export default {
+  components: {
+    codemirror,
+  },
+
+  data() {
+    return {
+      content: cm.testString,
+    };
+  },
+
+  methods: {
+    codeChange(newCode) {
+      cm.testString = newCode;
+      /* eslint no-console: 0 */
+      // console.log(cm.testString);
+    },
+  },
+};
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
