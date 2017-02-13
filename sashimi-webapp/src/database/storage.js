6a6df1b256978c460ec823ee019c2028806d5bd1
Daphne <won.jr@hotmail.com>
2017-02-13 09:20:25 +0800

    Add initializeDatabase function facade class

--- sashimi-webapp/src/database/storage.js
+++ sashimi-webapp/src/database/storage.js
@@ -1,5 +1,23 @@
-/*
- * CS3283/4 storage.js
- * This is a facade class for other components
+/**
+ *
+ *  CS3283/4 storage.js
+ *  This class acts as a facade for other developers to access to the database.
+ *  The implementation is a non-SQL local storage to support the WebApp.
+ *
  */
 
+const entitiesCreator = require('./create/entitiesCreator');
+
+class Storage {
+  static constructor() {}
+
+  static initializeDatabase() {
+    entitiesCreator.createUserTable();
+    entitiesCreator.createOrganizationTable();
+    entitiesCreator.createFolderTable();
+    entitiesCreator.createFileManagerTable();
+    entitiesCreator.clearDatabase();
+  }
+}
+
+Storage.initializeDatabase();
