d5f24dbf64672c4cff9fe58e85dd2f01cfb82c7d
tayjiehao <tayjiehao@hotmail.com>
2017-02-14 15:18:37 +0800

    remove unused test file

--- sashimi-webapp/test/unit/specs/logic/contentManipulator/contentManipulator.spec.js
+++
@@ -1,8 +0,0 @@
-const cm = require('src/logic/contentManipulator/contentManipulator');
-
-describe('ContentManipulator', () => {
-  it('can set() and get()', () => {
-    cm.set('test');
-    expect(cm.get()).to.equal('test');
-  });
-});
