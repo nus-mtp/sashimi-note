edc28a8458a46adbf01f0202104974e48f840a96
jiayingy <jiayinglek@gmail.com>
2017-02-19 18:19:03 +0800

    Shift viewer div container and relevant styles from Editor.vue to Viewer.vue

--- sashimi-webapp/src/components/Editor.vue
+++ sashimi-webapp/src/components/Editor.vue
@@ -9,9 +9,7 @@
         </div>
       </div>
       <div class="col span_6_of_12">
-        <div class="viewer">
-          <viewer></viewer>
-        </div>
+        <viewer></viewer>
       </div>
     </div>
   </div>
@@ -70,20 +68,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '../assets/styles/variables.scss';
 
-  .viewer {
-    height: calc(100vh - #{$navbar-height});
-    overflow-wrap: break-word;
-    overflow-y: scroll;
-    box-sizing: border-box;
-    margin: 0;
-    padding: 30px;
-    line-height: 1.6em;
-    font-size: 17px;
-
-    p {
-      margin: 5px;
-    }
-  }
 </style>
--- sashimi-webapp/src/components/Viewer.vue
+++ sashimi-webapp/src/components/Viewer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div v-html="getMarkdown">
+  <div class="viewer">
+    <div v-html="getMarkdown">
+    </div>
   </div>
 </template>
 
@@ -14,3 +16,22 @@
     }
   };
 </script>
+
+<style scoped lang='scss'>
+  @import '../assets/styles/variables.scss';
+  
+  .viewer {
+    height: calc(100vh - #{$navbar-height});
+    overflow-wrap: break-word;
+    overflow-y: scroll;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 30px;
+    line-height: 1.6em;
+    font-size: 17px;
+
+    p {
+      margin: 5px;
+    }
+  }
+</style>
