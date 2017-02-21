4418eba869c356bb580c9376b1a7f43d5575deee
JxJx <justjx@hotmail.com>
2017-02-21 17:58:30 +0800

    Implement Syntax Highlighting

--- sashimi-webapp/src/logic/documentPackager/documentFormatter.js
+++ sashimi-webapp/src/logic/documentPackager/documentFormatter.js
@@ -1,6 +1,6 @@
 /*
 ** HTML XSS Filtering plugin by leizongmin
-** using https://github.com/leizongmin/js-xss
+** https://github.com/leizongmin/js-xss
 **
 ** Features:
 ** 1. Specifies HTML tags and their attributes allowed with whitelist
@@ -8,10 +8,19 @@
 */
 const xssFilter = require('xss');
 
+// Customise whitelist
+xssFilter.whiteList.pre.push('class');
+xssFilter.whiteList.code.push('class');
+xssFilter.whiteList.span.push('class');
+
+const options = {
+  whiteList: xssFilter.whiteList
+};
+
 const documentFormatter = {
   format: function format(data) {
     // Filter out any possible XSS threat
-    data = xssFilter(data);
+    data = xssFilter(data, options);
 
     // Return data as it is for now.
     return data;
--- sashimi-webapp/src/logic/documentPackager/markdownProcessor.js
+++ sashimi-webapp/src/logic/documentPackager/markdownProcessor.js
@@ -1,9 +1,22 @@
+// Import highlight.js module for code highlighting
+const hljs = require('highlight.js'); // https://highlightjs.org/
+
 // Import Markdown-It module for markdown processing
 const md = require('markdown-it')({
   html: true,
   breaks: true,
   linkify: true,
-  typographer: true
+  typographer: true,
+  highlight(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        const val = hljs.highlight(lang, str, true).value;
+        return `<pre class="hljs"><code>${val}</code></pre>`;
+      } catch (__) { return ''; }
+    }
+
+    return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+  }
 });
 
 const validateData = function validateData(data) {
