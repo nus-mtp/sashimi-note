b7a4e3d5ef2378ab47970847bd2a7cd30d00f2c6
jiayingy <jiayinglek@gmail.com>
2017-02-21 16:48:51 +0800

    Change routing to FileManager

--- sashimi-webapp/src/router/index.js
+++ sashimi-webapp/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Content from 'components/Content';
+import FileManager from 'components/FileManager';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Content',
-      component: Content,
+      name: 'fileManager',
+      component: FileManager,
     },
   ],
 });
