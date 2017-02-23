a2ac0e363591a1725d3a0ad8c62aef84cc36b25f
jiayingy <jiayinglek@gmail.com>
2017-02-23 18:39:27 +0800

    Separate Navbar and UserInputs component from FileManager

---
+++ sashimi-webapp/src/components/file-manager/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="section group navbar">
+    <div class="col button-back vertical-align-child">
+      <button class="navbar-buttons">
+        <img src="../../assets/images/buttons/button-back.svg" class="button-img" alt="back">
+      </button>      
+    </div>
+    <div class="col">
+      <ul class="breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Blah</a></li>
+        <li>Blah</li>
+      </ul>
+    </div>
+    
+    <div class="float-right">
+      <div class="col vertical-align-child button-margin">
+        <button disabled class="navbar-buttons">
+          <img src="../../assets/images/buttons/button-delete.svg" class="button-img" alt="delete">
+        </button>
+        <button disabled class="navbar-buttons">
+          <img src="../../assets/images/buttons/button-duplicate.svg" class="button-img" alt="duplicate">
+        </button>
+        <button disabled class="navbar-buttons">
+          <img src="../../assets/images/buttons/button-download.svg" class="button-img" alt="download">
+        </button>
+      </div>
+      <div class="col vertical-align-child">
+        <button class="navbar-buttons">
+          <img src="../../assets/images/buttons/button-add-new.svg" class="button-img" alt="add">
+        </button>
+        <button class="navbar-buttons">
+          <img src="../../assets/images/buttons/button-upload-file.svg" class="button-img" alt="split-view">
+        </button>
+        <button class="navbar-buttons">
+          <img src="../../assets/images/buttons/button-filter.svg" class="button-img" alt="filter">
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss">
+ul {
+  list-style: none;
+  margin-top: 10px;
+
+  &.breadcrumb li {
+    display: inline;
+
+    &+li:before {
+      padding: 8px;
+      color: black;
+      content: "/\00a0";
+    }
+  }
+}
+
+button:disabled {
+  opacity: 0.2;
+}
+</style>
---
+++ sashimi-webapp/src/components/file-manager/UserInputs.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="userInputs">
+    <div class="searchBar inline-block">
+      <input type="text" placeholder="Search..." @changed="searchInput">
+    </div>
+    <div class="toggle-view inline-block">
+      <button class="navbar-buttons">
+        <span>ICON</span>
+      </button>
+      |
+      <button class="navbar-buttons">
+        <span>List</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+};
+
+</script>
+
+<style lang="scss">
+@import '../../assets/styles/variables.scss';
+
+.userInputs {
+  overflow: hidden;
+  text-align: center;
+  margin: 60px 0 30px 0;
+  position: relative;
+
+  input {
+    width: 30vw;
+    border: 1px solid black;
+    font-family: $font;
+    font-size: 18px;
+    padding: 8px 10px;
+  }
+
+  .searchBar {
+  }
+
+  .toggle-view {
+    padding: 4px;
+  }
+
+  span {
+    font-family: $font;
+    font-size: 16px;
+  }
+}
+</style>
