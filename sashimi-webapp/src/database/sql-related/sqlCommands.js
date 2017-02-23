c707aa88a1b0a998719878394ff364d5aaed90fa
Daphne <won.jr@hotmail.com>
2017-02-23 11:25:41 +0800

    Add link to common stringConcatenation function with sqlCommands with proper callback
    [1] callback function is an empty callback
    [2] add stringConcatenation to form string for alasql

--- sashimi-webapp/src/database/sql-related/sqlCommands.js
+++ sashimi-webapp/src/database/sql-related/sqlCommands.js
@@ -6,18 +6,25 @@ const alasql = new Alasql.Database();
 
 const stringManipulator = new StringManipulator();
 
+
+const callback = function callback() {
+  // do nothing??
 };
 
 export default function SqlCommands() {
   this.getFullTableData = function getFullTableData(tableName) {
+    return alasql(stringManipulator.stringConcat('SELECT * FROM ', tableName), callback());
   };
 
   this.createTable = function createTable(sqlStatement) {
+    alasql(stringManipulator.stringConcat('CREATE TABLE IF NOT EXISTS ', sqlStatement), callback());
   };
 
   this.deleteTable = function deleteTable(tableName) {
+    alasql(stringManipulator.stringConcat('DROP TABLE ', tableName), callback());
   };
 
   this.cleanTable = function cleanTable(tableName) {
+    alasql(stringManipulator.stringConcat('DELETE * FROM ', tableName), callback());
   };
 }
