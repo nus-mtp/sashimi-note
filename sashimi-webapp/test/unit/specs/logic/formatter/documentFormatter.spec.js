8dac84f090dba6466901def2c5fd67e494349b46
amoshydra <amoswwj@gmail.com>
2017-02-23 18:28:23 +0800

    Update documentFormatter test spec for new method calls

--- sashimi-webapp/test/unit/specs/logic/formatter/documentFormatter.spec.js
+++ sashimi-webapp/test/unit/specs/logic/formatter/documentFormatter.spec.js
@@ -1,8 +1,28 @@
-const documentFormatter = require('src/logic/wrapper/documentFormatter');
+import documentFormatter from 'src/logic/wrapper/documentFormatter';
+import base64OfSimplePdf from './reference/base64OfSimplePdf';
 
 describe('DocumentFormatter', () => {
-  it('returns the same data', () => {
-    const test1 = documentFormatter.format('test');
-    expect(test1).to.equal('test');
+  it('should returns the same data for \'html viewMode\'', () => {
+    const viewMode = 'html';
+    const inputData = '<div>This is a HTML content</div>';
+    const outputData = documentFormatter.format(inputData, viewMode);
+
+    expect(outputData).to.equal('<div>This is a HTML content</div>');
+  });
+
+  it('should returns a pdfBlob for \'pages viewMode\'', () => {
+    const expectedOutput = base64OfSimplePdf;
+    const inputData = '<div>This is a HTML content</div>';
+
+    const thatPromise = documentFormatter.getPdfBlob(inputData);
+
+    thatPromise.then((outputData) => {
+      // pdfJS does not give deterministic outputData
+      // around 5 characters will always be different in the output
+      // therefore, an approximation method to use content length for
+      // testing is used.
+
+      expect(outputData.length).to.equal(expectedOutput.length);
+    });
   });
 });
