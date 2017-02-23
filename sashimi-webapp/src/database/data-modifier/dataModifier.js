ff54641bdac325b3a6c0566beed149a87b08d2d6
Daphne <won.jr@hotmail.com>
2017-02-23 10:14:17 +0800

    Add dataModifier facade class for all data modifying roles AND a empty function deleteAllEntities

---
+++ sashimi-webapp/src/database/data-modifier/dataModifier.js
@@ -0,0 +1,13 @@
+/*
+ * CS3283/4 dataModifier.js
+ * This acts as a facade class for modifying and update data for storage facade
+ *
+ */
+
+
+export default class dataModifier {
+  static constructor() {}
+
+  static deleteAllEntities() {
+  }
+}
