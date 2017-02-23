69071847ce857d7d5a2de2f62b95a9485b996d20
Daphne <won.jr@hotmail.com>
2017-02-23 10:18:07 +0800

    Add DateTime function: 1st function: Generate current date and time

---
+++ sashimi-webapp/src/database/generated-data/DateTime.js
@@ -0,0 +1,18 @@
+import StringManipulator from '../StringManipulation';
+
+const date = new Date();
+
+const stringManipulator = new StringManipulator();
+
+export default function dateTime() {
+  this.getCurrentDateTime = function getCurrentDateTime() {
+    const year = date.getFullYear();
+    const month = stringManipulator.stringDateTime00Format(date.getMonth());
+    const day = stringManipulator.stringDateTime00Format(date.getDate());
+    const hour = stringManipulator.stringDateTime00Format(date.getHours());
+    const minute = stringManipulator.stringDateTime00Format(date.getMinutes());
+    const second = stringManipulator.stringDateTime00Format(date.getSeconds());
+
+    return stringManipulator.stringConcat(year, '.', month, '.', day, ' ', hour, ':', minute, ':', second);
+  };
+}
