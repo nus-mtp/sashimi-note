4913c97bcec00cceb61a33900f307a4a6c153807
Daphne <won.jr@hotmail.com>
2017-02-23 10:17:29 +0800

    Add StringManipulation function to modify DateTime from xx to 0x format
    This is to ensure it follows alasql format for date

--- sashimi-webapp/src/database/StringManipulation.js
+++ sashimi-webapp/src/database/StringManipulation.js
@@ -4,4 +4,12 @@ export default function StringManipulation() {
     return stringToConcat.join('');
   };
 
+  this.stringDateTime00Format = function stringDateTime00Format(dateTimeNumber) {
+    if (typeof dateTimeNumber == 'number') {
+      if (dateTimeNumber < 10) {
+        return this.stringConcat('0', dateTimeNumber);
+      }
+    }
+    return dateTimeNumber;
+  };
 }
