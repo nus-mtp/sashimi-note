aa9e0cd56e0823684536668766f01a231ab05c43
amoshydra <amoswwj@gmail.com>
2017-02-26 17:45:42 +0800

    Refactor jsPdf.fromHTML function call code

--- sashimi-webapp/src/logic/documentPackager/documentFormatter.js
+++ sashimi-webapp/src/logic/documentPackager/documentFormatter.js
@@ -1,41 +1,67 @@
 import JsPDF from 'jspdf';
 import base64 from 'src/helpers/base64';
 
-const defaultPageSize = {
-  orientation: 'portrait',
-  unit: 'mm',
-  format: 'a4'
+const defaultRenderConfig = {
+  paperSize: {
+    orientation: 'portrait',
+    unit: 'mm',
+    format: 'a4'
+  },
+  width: 170
 };
 
 /**
   * Convert HTML string into a jsPDF output
   * @param {string} htmlString - HTML string
-  * @param {object} paperSize
+  * @param {object} renderConfig - contain the paper size and text width
   * @return {Promise<jsPDFDoc>} jsPDF output
   */
 const createPdfFromHtml = function createPdfFromHtml(htmlString, renderConfig) {
   const processedHtmlString = htmlString || '<div></div>';
 
+  renderConfig = renderConfig || defaultRenderConfig;
+
   return new Promise((resolve, reject) => {
-    const paperSize = renderConfig.paperSize || defaultPageSize;
-    const doc = new JsPDF(paperSize);
+    const doc = new JsPDF(renderConfig.paperSize);
 
     // Beta Stage API provided by JsPDF
     // Source code can be viewed at:
     // https://github.com/MrRio/jsPDF/blob/master/plugins/from_html.js
 
-    doc.fromHTML(processedHtmlString, 10, 0, {
-      width: renderConfig.width
-    }, (dispose) => {
-      // dispose: object with X, Y of the last line add to the PDF
-      //          this allow the insertion of new lines after html
-      if (dispose) {
-        resolve(doc.output());
-      } else {
-        // dispose will be undefined or false is jsPDF failed to render
-        reject('Error creating pdf with jsPDF');
-      }
-    }, { top: 20, bottom: 20 });
+    const pdfData = {
+      html: processedHtmlString,
+      offset: {
+        x: 12,
+        y: 12
+      },
+      sizing: {
+        width: renderConfig.width
+      },
+      margin: {
+        top: 0,
+        bottom: 0
+      },
+      callback: ((dispose) => {
+        // dispose: object with X, Y of the last line add to the PDF
+        //          this allow the insertion of new lines after html
+        if (dispose) {
+          resolve(doc.output());
+        } else {
+          // dispose will be undefined or false is jsPDF failed to render
+          reject('Error creating pdf with jsPDF');
+        }
+      })
+    };
+
+    // Generate PDF from the data above
+    doc.fromHTML(
+      pdfData.html,
+      pdfData.offset.x,
+      pdfData.offset.y,
+      pdfData.sizing,
+      pdfData.callback,
+      pdfData.margin
+    );
   });
 };
 
