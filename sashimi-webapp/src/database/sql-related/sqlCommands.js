0a0226df358e10d1bebcc9486b6f46b76d8505cc
tharain <won.jr@hotmail.com>
2017-02-27 00:43:43 +0800

    Add getFullTableData implementation to sqlCommands.js

--- sashimi-webapp/src/database/sql-related/sqlCommands.js
+++ sashimi-webapp/src/database/sql-related/sqlCommands.js
@@ -26,7 +26,16 @@ export default function sqlCommands() {
   };
 
   this.getFullTableData = function getFullTableData(tableName) {
-    return alasql(stringManipulator.stringConcat('SELECT * FROM ', tableName), callback());
+    // ensure working in browsers that support Promise
+    if (typeof Promise === 'function') {
+      return new Promise((resolve, reject) => {
+        alasql.promise([stringManipulator.stringConcat('SELECT * FROM ', tableName)])
+          .then(data => resolve(data))
+          .catch(sqlError => sqlError);
+      });
+    } else {
+      throw new exceptions.PromiseFunctionNotDefined();
+    }
   };
 
   this.createTable = function createTable(sqlStatement) {
