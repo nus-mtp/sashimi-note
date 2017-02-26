8fddc92424335d2f6ee703c965d2ddf61160c853
tharain <won.jr@hotmail.com>
2017-02-27 01:09:03 +0800

    Add createNewFile implementations to dataModifier.js

--- sashimi-webapp/src/database/data-modifier/dataModifier.js
+++ sashimi-webapp/src/database/data-modifier/dataModifier.js
@@ -6,11 +6,28 @@
 
 import dataAdd from './dataAdd';
 import dataDelete from './dataDelete';
+import dataUpdate from './dataUpdate';
+import exceptions from '../exceptions';
 
 export default class dataModifier {
   static constructor() {}
 
   static deleteAllEntities() {
     dataDelete.deleteAllEntities();
+  }
+
+  static createNewFile(organizationId, filePath, folderId) {
+    if (typeof Promise === 'function') {
+      return new Promise((resolve, reject) => {
+        dataAdd.createNewFile(organizationId, filePath, folderId)
+          .then((fileId) => {
+            console.log('DATAMODIFIER');
+            console.log(fileId);
+            resolve(fileId);
+          }).catch(sqlErr => sqlErr);
+      });
+    } else {
+      throw new exceptions.PromiseFunctionNotDefined();
+    }
   }
 }
