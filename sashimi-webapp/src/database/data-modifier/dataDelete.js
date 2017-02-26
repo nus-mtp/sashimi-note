b48645900041b645e9ca79f8e461af4bad950373
tharain <won.jr@hotmail.com>
2017-02-27 01:07:27 +0800

    Add dataDelete.js and implementations
    [1] update deleteAllEntities constant name
    [2] implemented deleteFile
    [3] implemented deleteFolder

--- sashimi-webapp/src/database/data-modifier/dataDelete.js
+++ sashimi-webapp/src/database/data-modifier/dataDelete.js
@@ -1,5 +1,7 @@
 import constants from '../constants';
 
+import exceptions from '../exceptions';
+
 import SqlCommands from '../sql-related/sqlCommands';
 
 const sqlCommands = new SqlCommands();
@@ -8,8 +10,29 @@ export default class dataDelete {
   static constructor() {}
 
   static deleteAllEntities() {
-    for (let index = 0; index < constants.arrayEntitiesName.length; index += 1) {
-      sqlCommands.deleteTable(constants.arrayEntitiesName[index]);
+    for (let index = 0; index < constants.ARRAY_ENTITIES_NAME.length; index += 1) {
+      sqlCommands.deleteTable(constants.ARRAY_ENTITIES_NAME[index]);
+    }
+  }
+
+  static deleteFile(fileId) {
+    if (typeof Promise === 'function') {
+      return new Promise((resolve, reject) => {
+        resolve(sqlCommands.deleteFile(fileId));
+      });
+    } else {
+      throw new exceptions.PromiseFunctionNotDefined();
+    }
+  }
+
+
+  static deleteFolder(folderId) {
+    if (typeof Promise === 'function') {
+      return new Promise((resolve, reject) => {
+        resolve(sqlCommands.deleteFolder(folderId));
+      });
+    } else {
+      throw new exceptions.PromiseFunctionNotDefined();
     }
   }
 }
