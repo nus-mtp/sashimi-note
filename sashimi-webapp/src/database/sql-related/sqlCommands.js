f34d2491eb848a4a1c761729e403d28cbfed6727
tharain <won.jr@hotmail.com>
2017-02-27 00:46:42 +0800

    Add partialSearchFileName implementation into sqlCommands

--- sashimi-webapp/src/database/sql-related/sqlCommands.js
+++ sashimi-webapp/src/database/sql-related/sqlCommands.js
@@ -57,6 +57,20 @@ export default function sqlCommands() {
       throw new exceptions.PromiseFunctionNotDefined();
     }
   };
+
+  this.partialSearchFileName = function partialSearchFileName(searchString) {
+    if (typeof Promise === 'function') {
+      return new Promise((resolve, reject) => {
+        alasql.promise([stringManipulator.stringConcat('SELECT * FROM ', constants.ENTITIES_FILE_MANAGER,
+                                                       ' WHERE ', constants.HEADER_FILE_MANAGER_FILE_NAME,
+                                                       ' LIKE "%', searchString, '%"')])
+        .then(data => resolve(data))
+        .catch(sqlError => sqlError);
+      });
+    } else {
+      throw new exceptions.PromiseFunctionNotDefined();
+    }
+  };
   };
 
   this.deleteTable = function deleteTable(tableName) {
