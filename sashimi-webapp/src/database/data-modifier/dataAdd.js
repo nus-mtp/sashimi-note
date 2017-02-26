c70ed67b450e6c6bde67201a73bccf8c6f9639a9
tharain <won.jr@hotmail.com>
2017-02-27 01:01:15 +0800

    Add supporting functions into dataAdd.js
    [1] getFormattedCurrentDateTime to suit alasql
    [2] createNewFile that create a default file
    [3] createNewFolder that create a default folder

--- sashimi-webapp/src/database/data-modifier/dataAdd.js
+++ sashimi-webapp/src/database/data-modifier/dataAdd.js
@@ -1,3 +1,83 @@
+import exceptions from '../exceptions';
+
+import constants from '../constants';
+
+import SqlCommands from '../sql-related/sqlCommands';
+
+import DateTime from '../generated-data/dateTime';
+
+import StringManipulator from '../stringManipulation';
+
+const sqlCommands = new SqlCommands();
+const dateTime = new DateTime();
+const stringManipulator = new StringManipulator();
+
+function getFormattedCurrentDateTime() {
+  return stringManipulator.stringConcat('"', dateTime.getCurrentDateTime(), '"');
+}
+
+function createNewFile(organizationId, filePath, folderId, newFileId) {
+  if (typeof Promise === 'function') {
+    return new Promise((resolve, reject) => {
+      const thisOrganizationId = organizationId;
+      const thisFolderId = folderId;
+      const fileId = newFileId;
+      const fileName = constants.DEFAULT_FILE_NAME;
+      const fileMarkDown = '""';
+      const filePermission = constants.PERMISSION_CREATOR;
+      const fileCreationDate = getFormattedCurrentDateTime();
+      const fileLastModified = fileCreationDate;
+      const fileNewPath = stringManipulator.stringConcat('"', filePath, '"');
+
+      sqlCommands.insertContent(constants.ENTITIES_FILE_MANAGER,
+                                thisOrganizationId,
+                                thisFolderId,
+                                fileId,
+                                fileName,
+                                fileMarkDown,
+                                filePermission,
+                                fileCreationDate,
+                                fileLastModified,
+                                fileNewPath)
+      .then(data => fileId)
+      .catch(err => err);
+    });
+  } else {
+    throw new exceptions.PromiseFunctionNotDefined();
+  }
+}
+
+function createNewFolder(organizationId, folderPath, currentFolderId, newFolderId) {
+  if (typeof Promise === 'function') {
+    return new Promise((resolve, reject) => {
+      const formattedCurrentDateTime = getFormattedCurrentDateTime();
+
+      const folderId = newFolderId;
+      const parentFolderId = currentFolderId;
+      const permissionIndex = constants.PERMISSION_CREATOR;
+      const thisOrganizationId = organizationId;
+      const creationDate = formattedCurrentDateTime;
+      const folderName = constants.DEFAULT_FOLDER_NAME;
+      const lastModifiedDate = creationDate;
+      const thisFolderPath = folderPath;
+
+      sqlCommands.insertContent(constants.ENTITIES_FOLDER,
+                                folderId,
+                                parentFolderId,
+                                permissionIndex,
+                                thisOrganizationId,
+                                creationDate,
+                                folderName,
+                                lastModifiedDate,
+                                thisFolderPath)
+      .then(data => true)
+      .catch(err => err);
+    });
+  } else {
+    throw new exceptions.PromiseFunctionNotDefined();
+  }
+}
+
 export default class dataAdd {
   static constructor() {}
 }
