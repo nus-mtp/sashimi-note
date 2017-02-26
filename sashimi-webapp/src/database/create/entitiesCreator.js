899f54db185ff0c79ac620163a265627c8015c55
tharain <won.jr@hotmail.com>
2017-02-27 01:04:20 +0800

    Fix PrimaryKey and ForeignKey in entitiesCreator
    [1] external library alasql have some bug with foreignKey, therefore
    removed.
    [2] primary key update to the correct key

--- sashimi-webapp/src/database/create/entitiesCreator.js
+++ sashimi-webapp/src/database/create/entitiesCreator.js
@@ -42,11 +42,8 @@ export default class entitiesCreator {
       tableCreator.addHeader(constants.HEADER_ORGANIZATION_PARENT_ORGANIZATION_ID, 'NUMBER');
 
       tableCreator.setForeignKey(constants.HEADER_ORGANIZATION_USER_ID,
-                                  constants.ENTITIES_USER,
-                                  constants.HEADER_USER_USER_ID);
-      tableCreator.setForeignKey(constants.HEADER_ORGANIZATION_PARENT_ORGANIZATION_ID,
-                                  constants.ENTITIES_ORGANIZATION,
-                                  constants.HEADER_ORGANIZATION_ORGANIZATION_ID);
+                                 constants.ENTITIES_USER,
+                                 constants.HEADER_USER_USER_ID);
 
       tableCreator.setPrimaryKeys(constants.HEADER_ORGANIZATION_ORGANIZATION_ID,
                                     constants.HEADER_ORGANIZATION_USER_ID);
@@ -69,10 +66,6 @@ export default class entitiesCreator {
       tableCreator.addHeader(constants.HEADER_FILE_MANAGER_PERMISSION_INDEX, 'NUMBER');
       tableCreator.addHeader(constants.HEADER_FILE_MANAGER_CREATION_DATE, 'DATE');
       tableCreator.addHeader(constants.HEADER_FILE_MANAGER_LAST_MODIFIED_DATE, 'DATE');
-
-      tableCreator.setForeignKey(constants.HEADER_FILE_MANAGER_ORGANIZATION_ID,
-                                  constants.ENTITIES_ORGANIZATION,
-                                  constants.HEADER_ORGANIZATION_ORGANIZATION_ID);
 
       tableCreator.setPrimaryKeys(constants.HEADER_FILE_MANAGER_FILE_ID,
                                     constants.HEADER_FILE_MANAGER_ORGANIZATION_ID);
@@ -97,15 +90,8 @@ export default class entitiesCreator {
       tableCreator.addHeader(constants.HEADER_FOLDER_FOLDER_NAME, 'NUMBER');
       tableCreator.addHeader(constants.HEADER_FOLDER_LAST_MODIFIED_DATE, 'DATE');
 
-      tableCreator.setForeignKey(constants.HEADER_FOLDER_PARENT_FOLDER_ID,
-                                  constants.ENTITIES_FOLDER,
-                                  constants.HEADER_FOLDER_FOLDER_ID);
-      tableCreator.setForeignKey(constants.HEADER_FOLDER_ORGANIZATION_ID,
-                                  constants.ENTITIES_ORGANIZATION,
-                                  constants.HEADER_ORGANIZATION_ORGANIZATION_ID);
-
-      tableCreator.setPrimaryKeys(constants.HEADER_ORGANIZATION_ORGANIZATION_ID,
-                                    constants.HEADER_ORGANIZATION_USER_ID);
+      tableCreator.setPrimaryKeys(constants.HEADER_FOLDER_ORGANIZATION_ID,
+                                    constants.HEADER_FOLDER_FOLDER_ID);
       const isTableCreatedSuccessful = tableCreator.endCreateTable();
 
       return isTableCreatedSuccessful;
