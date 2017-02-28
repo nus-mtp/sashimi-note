d1eb2ec6c967155389ed2630e05f255c899390f1
jiayingy <jiayinglek@gmail.com>
2017-02-28 13:17:55 +0800

    Prevent img from being draggable

--- sashimi-webapp/src/App.vue
+++ sashimi-webapp/src/App.vue
@@ -33,6 +33,15 @@ body {
   color: black;
 }
 
+img {
+  user-drag: none; 
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+}
+
 :focus {
   outline: 0;
 }
