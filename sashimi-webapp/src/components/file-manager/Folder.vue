97905c5b66169264141ca42e446535101455b778
jiayingy <jiayinglek@gmail.com>
2017-03-01 01:52:33 +0800

    Remove unused button tag and rename methods and variables containing "file" to "folder"

--- sashimi-webapp/src/components/file-manager/Folder.vue
+++ sashimi-webapp/src/components/file-manager/Folder.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="col folder vertical-align-child" v-on:dblclick="openFile()" id="123">
-    <button class="button-folder">
-      <img src="../../assets/images/icons/icon-folder.svg" alt="folder">
-      <p class="inline-block">CS3244 Project dsajdakhdsajkdhaajskdhasjdhasjhdjashdjahdajhdjahdjkahsdajshdjahdjahskajhdksahdkajshdksjahdkajshdsadajshdkajsdas hdjask dakshdjaksd kasdjkashdkjsahdjkhas dhaskjdkjsdjsdjkahskjhsadajk d</p>
-    </button>
+  <div class="col folder vertical-align-child" v-on:dblclick="openFolder()" id="123">
+    <img src="../../assets/images/icons/icon-folder.svg" alt="folder">
+    <p class="inline-block">CS3244 Project dsajdakhdsajkdhaajskdhasjdhasjhdjashdjahdajhdjahdjkahsdajshdjahdjahskajhdksahdkajshdksjahdkajshdsadajshdkajsdas hdjask dakshdjaksd kasdjkashdkjsahdjkhas dhaskjdkjsdjsdjkahskjhsadajk d</p>
   </div>
 </template>
 
 <script>
   export default {
     methods: {
-      openFile() {
-        const fileId = this.$el.id;
-        console.log(fileId);
-        this.$router.push({ path: 'content', query: { id: fileId } });
+      openFolder() {
+        const folderId = this.$el.id;
+        console.log(folderId);
+        this.$router.push({ path: 'content', query: { id: folderId } });
       },
     }
   };
@@ -22,11 +20,4 @@
 <style scoped lang="scss">
 @import 'src/assets/styles/variables.scss';
 
-.button-folder {
-  border: none;
-  background-color: transparent;
-  font-family: $general-font;
-  padding: 0;
-  z-index: 999;
-}
 </style>
