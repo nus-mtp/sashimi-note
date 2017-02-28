3bad9f9316258c74b4e97f9e993558daf5e349d0
jiayingy <jiayinglek@gmail.com>
2017-03-01 01:51:51 +0800

    Enable redirect to editor/viewer page on dblclick

--- sashimi-webapp/src/components/file-manager/File.vue
+++ sashimi-webapp/src/components/file-manager/File.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="col file vertical-align-child">
+  <div class="col file vertical-align-child" v-on:dblclick="openFile()" id="123">
     <img src="../../assets/images/icons/icon-file.svg" alt="file">
     <p class="inline-block">CS3kjs h</p>
   </div>
 </template>
 
 <script>
-
+export default {
+  methods: {
+    openFile() {
+      const fileId = this.$el.id;
+      console.log(fileId);
+      this.$router.push({ path: 'content', query: { id: fileId } });
+    },
+  }
+};
 </script>
 
 <style scoped lang="scss">
