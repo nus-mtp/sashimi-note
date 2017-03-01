aa47a47c9e5efb29b8890099d9471ac8b80c3987
amoshydra <amoswwj@gmail.com>
2017-03-01 15:40:26 +0800

    Include documentation for renderer helper module

--- sashimi-webapp/src/logic/renderer/helper.js
+++ sashimi-webapp/src/logic/renderer/helper.js
@@ -1,12 +1,20 @@
 import unitConverter from 'src/helpers/unitConverter';
 
 export default {
+  /**
+   * Overwrite existing node's style with the given style
+   */
   overwriteStyle: function overwriteStyle(target, source) {
     Object.keys(source).forEach((styleKey) => {
       target[styleKey] = source[styleKey];
     });
   },
 
+  /**
+   * Complete the render height of the given page by
+   * substracting the page height with the top and bottom
+   * paddings
+   */
   computeRenderHeight: function computeRenderHeight(page) {
     return parseFloat(unitConverter.get(page.height, 'px'), 10) - (
            parseFloat(unitConverter.get(page.padding.top, 'px'), 10) +
