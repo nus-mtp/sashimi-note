1dfa133f937401ea7bbaeff3c6b507cdf6f48170
amoshydra <amoswwj@gmail.com>
2017-03-02 14:40:15 +0800

    Forcefully insert viewer-container into testing browser

--- sashimi-webapp/test/unit/specs/Content.spec.js
+++ sashimi-webapp/test/unit/specs/Content.spec.js
@@ -3,20 +3,14 @@ import Content from 'src/components/editor-viewer/Content';
 
 const VIEWER_CONTAINER_ID = 'viewer-container';
 
+const mockContainer = document.createElement('DIV');
+mockContainer.setAttribute('id', VIEWER_CONTAINER_ID);
+document.body.appendChild(mockContainer);
+
 describe('Content.vue', () => {
   // A mock viewer-container DOM is needed for this test.
   // It will be created before all the test are ran, and remove
   // when all the tests are finished.
-  before(() => {
-    const mockContainer = document.createElement('DIV');
-    mockContainer.setAttribute('id', VIEWER_CONTAINER_ID);
-    document.body.appendChild(mockContainer);
-  });
-
-  after(() => {
-    const mockContainer = document.getElementById(VIEWER_CONTAINER_ID);
-    if (mockContainer) { mockContainer.remove(); }
-  });
 
   it('should render Editor component', () => {
     const Constructor = Vue.extend(Content);
