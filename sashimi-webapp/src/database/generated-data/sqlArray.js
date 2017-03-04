24e0535309015f3878951faad677807706b0ee21
tharain <won.jr@hotmail.com>
2017-03-04 12:27:18 +0800

    Add new function file sqlArray

---
+++ sashimi-webapp/src/database/generated-data/sqlArray.js
@@ -0,0 +1,8 @@
+/*
+ *  The purpose of this function is to allow initDataGenerator.js to create array
+ *  of array of object that suits alasql to create default data in nonsql indexeddb
+ *
+ */
+
+export default function sqlArray() {
+}
