be89ebbf23b6f404d90004e4ba61688525807b0e
tharain <won.jr@hotmail.com>
2017-03-06 00:54:17 +0800

    Fix dataAdd.js to suit alasql dependencies
    [1] correct the indentation
    [2] use sqlArray in createNewFile and createNewFolder
    [3] remove unwanted dependency stringManipulation
    [4] add new dependency sqlArray

--- sashimi-webapp/src/database/data-modifier/dataAdd.js
+++ sashimi-webapp/src/database/data-modifier/dataAdd.js
@@ -6,40 +6,41 @@ import SqlCommands from '../sql-related/sqlCommands';
 
 import DateTime from '../generated-data/dateTime';
 
-import StringManipulator from '../stringManipulation';
+import SqlArray from '../generated-data/sqlArray';
 
 const sqlCommands = new SqlCommands();
 const dateTime = new DateTime();
-const stringManipulator = new StringManipulator();
-
-function getFormattedCurrentDateTime() {
-  return stringManipulator.stringConcat('"', dateTime.getCurrentDateTime(), '"');
-}
+const alasqlArray = new SqlArray();
 
 function createNewFile(organizationId, filePath, folderId, newFileId) {
   if (typeof Promise === 'function') {
     return new Promise((resolve, reject) => {
-      const thisOrganizationId = organizationId;
-      const thisFolderId = folderId;
+      const currentDateTime = dateTime.getCurrentDateTime();
+
+      const fileOrganizationId = organizationId;
+      const fileFolderId = folderId;
       const fileId = newFileId;
       const fileName = constants.DEFAULT_FILE_NAME;
-      const fileMarkDown = '""';
+      const fileMarkDown = constants.DEFAULT_EMPTY;
       const filePermission = constants.PERMISSION_CREATOR;
-      const fileCreationDate = getFormattedCurrentDateTime();
-      const fileLastModified = fileCreationDate;
-      const fileNewPath = stringManipulator.stringConcat('"', filePath, '"');
+      const fileCreationDateTime = currentDateTime;
+      const fileLastModified = fileCreationDateTime;
+      const fileThisPath = filePath;
 
-      return sqlCommands.insertContent(constants.ENTITIES_FILE_MANAGER,
-                                thisOrganizationId,
-                                thisFolderId,
-                                fileId,
-                                fileName,
-                                fileMarkDown,
-                                filePermission,
-                                fileCreationDate,
-                                fileLastModified,
-                                fileNewPath)
-      .then(data => resolve(data))
+      alasqlArray.initializeAlasqlArray();
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_ORGANIZATION_ID, fileOrganizationId);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_FOLDER_ID, fileFolderId);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_FILE_ID, fileId);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_FILE_NAME, fileName);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_FILE_MARKDOWN, fileMarkDown);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_PERMISSION_INDEX, filePermission);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_CREATION_DATE, fileCreationDateTime);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_LAST_MODIFIED_DATE, fileLastModified);
+      alasqlArray.addKeyBasePair(constants.HEADER_FILE_MANAGER_PATH, fileThisPath);
+      const newFileData = alasqlArray.endAlasqlArray();
+
+      return sqlCommands.insertContent(constants.ENTITIES_FILE_MANAGER, newFileData)
+      .then(success => resolve(true))
       .catch(err => reject(err));
     });
   } else {
@@ -50,27 +51,30 @@ function createNewFile(organizationId, filePath, folderId, newFileId) {
 function createNewFolder(organizationId, folderPath, currentFolderId, newFolderId) {
   if (typeof Promise === 'function') {
     return new Promise((resolve, reject) => {
-      const formattedCurrentDateTime = getFormattedCurrentDateTime();
+      const currentDateTime = dateTime.getCurrentDateTime();
 
       const folderId = newFolderId;
-      const parentFolderId = currentFolderId;
-      const permissionIndex = constants.PERMISSION_CREATOR;
-      const thisOrganizationId = organizationId;
-      const creationDate = formattedCurrentDateTime;
+      const folderParentFolderId = currentFolderId;
+      const folderpermissionIndex = constants.PERMISSION_CREATOR;
+      const folderOrganizationId = organizationId;
+      const folderCreationDate = currentDateTime;
       const folderName = constants.DEFAULT_FOLDER_NAME;
-      const lastModifiedDate = creationDate;
-      const thisFolderPath = folderPath;
+      const folderlastModifiedDate = folderCreationDate;
+      const folderThisPath = folderPath;
 
-      return sqlCommands.insertContent(constants.ENTITIES_FOLDER,
-                                folderId,
-                                parentFolderId,
-                                permissionIndex,
-                                thisOrganizationId,
-                                creationDate,
-                                folderName,
-                                lastModifiedDate,
-                                thisFolderPath)
-      .then(data => resolve(true))
+      alasqlArray.initializeAlasqlArray();
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_FOLDER_ID, folderId);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_PARENT_FOLDER_ID, folderParentFolderId);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_PERMISSION_INDEX, folderpermissionIndex);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_ORGANIZATION_ID, folderOrganizationId);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_CREATION_DATE, folderCreationDate);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_FOLDER_NAME, folderName);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_LAST_MODIFIED_DATE, folderlastModifiedDate);
+      alasqlArray.addKeyBasePair(constants.HEADER_FOLDER_PATH, folderThisPath);
+      const newFolderData = alasqlArray.endAlasqlArray();
+
+      return sqlCommands.insertContent(constants.ENTITIES_FOLDER, newFolderData)
+      .then(success => resolve(true))
       .catch(err => reject(err));
     });
   } else {
@@ -89,8 +93,8 @@ export default class dataAdd {
           .then((maxId) => {
             const newFileId = maxId + 1;
             return createNewFile(organizationId, filePath, folderId, newFileId)
-              .then(data => resolve(data))
-              .catch(err => reject(err));
+            .then(data => resolve(data))
+            .catch(err => reject(err));
           }).catch(sqlError => reject(sqlError))
       );
     } else {
