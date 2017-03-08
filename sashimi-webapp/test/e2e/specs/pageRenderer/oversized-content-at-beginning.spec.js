3bb586892af4fb35be73282cc3920f728368f30f
Amos Wong <amoswwj@gmail.com>
2017-03-09 01:18:03 +0800

    Add test for oversized content

---
+++ sashimi-webapp/test/e2e/specs/pageRenderer/oversized-content-at-beginning.spec.js
@@ -0,0 +1,43 @@
+const textLoader = require('../../helpers/textLoader');
+
+module.exports = {
+  'navigate to /content': (browser) => {
+    const devServer = browser.globals.devServerURL;
+
+    browser
+      .url(`${devServer}/content`)
+      .waitForElementVisible('#app', 5000);
+  },
+
+  'verify the HTML in /content': (browser) => {
+    browser
+      .assert.elementPresent('.viewer')
+      .assert.elementPresent('#viewer-container')
+      .assert.elementPresent('#manage-file');
+  },
+
+  'change fileFormat to Pages mode': (browser) => {
+    browser
+      .click('#manage-file')
+      .pause(500)
+      .click('button[data-format="pages"]')
+      .pause(500)
+      .assert.elementPresent('.viewer[data-fileformat="pages"]');
+  },
+
+  'write to codemirror': (browser) => {
+    browser
+      .click('.CodeMirror')
+      .pause(500)
+      .keys(textLoader.load('references/oversized-content-at-beginning'), () => {
+        browser.pause(2000)
+        .assert.elementCount('.page-view', 3)
+        .assert.elementPresent('.page-view:nth-child(2) pre code');
+      });
+  },
+
+  'after': (browser) => {
+    browser.end();
+  },
+
+};
