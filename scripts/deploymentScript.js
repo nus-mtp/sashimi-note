a9749db9c682d3fbdbb3634185039d40f61933e0
amoshydra <amoswwj@gmail.com>
2017-03-08 11:50:22 +0800

    Remove build process for testing environment

--- scripts/deploymentScript.js
+++ scripts/deploymentScript.js
@@ -11,27 +11,28 @@ var webapp = {
 var statusBuild = -1;
 var statusStart = -1;
 
+// Don't need to build web app if NODE_ENV is testing
+if (process.env.NODE_ENV !== 'testing') {
+  printTitle('Build web application');
 
-printTitle('Build web application');
+  cd(`./${webapp.path}`);
+  exec('yarn');
+  statusBuild = exec('yarn run build').code;
+  throwErrorIfFailedToExec(statusBuild, 'build failed')
 
-cd(`./${webapp.path}`);
-exec('yarn');
-statusBuild = exec('yarn run build').code;
-throwErrorIfFailedToExec(statusBuild, 'build failed')
+  printTitle('Copy webapp to server folder');
 
+  mkdir('-p', webapp.buildPath);
+  mkdir('-p', `../${platform.path}/${platform.buildPath}`);
+  rm('-rf', `../${platform.path}/${platform.buildPath}`);
+  cp('-R', webapp.buildPath, `../${platform.path}`);
+  mv(`../${platform.path}/${webapp.buildPath}`, `../${platform.path}/${platform.buildPath}`);
 
+  ls(`../${platform.path}/${platform.buildPath}`).forEach(function(file) {
+    console.log(file);
+  });
+}
 
-printTitle('Copy webapp to server folder');
-
-mkdir('-p', webapp.buildPath);
-mkdir('-p', `../${platform.path}/${platform.buildPath}`);
-rm('-rf', `../${platform.path}/${platform.buildPath}`);
-cp('-R', webapp.buildPath, `../${platform.path}`);
-mv(`../${platform.path}/${webapp.buildPath}`, `../${platform.path}/${platform.buildPath}`);
-
-ls(`../${platform.path}/${platform.buildPath}`).forEach(function(file) {
-  console.log(file);
-});
 
 cd(`..`);
 
