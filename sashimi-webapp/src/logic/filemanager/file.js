109466a005d75907b00e1d086ff43a1ce9def5e2
tayjiehao <tayjiehao@hotmail.com>
2017-03-09 15:21:13 +0800

    implemented file related operations into file component.

--- sashimi-webapp/src/logic/filemanager/file.js
+++ sashimi-webapp/src/logic/filemanager/file.js
@@ -1,3 +1,6 @@
+// import storage from '../../database/storage';
+import Folder from './folder';
+
 /**
 * File Object
 *
@@ -5,11 +8,122 @@
 * @param {String} fileName
 * @param {String} filePath
 */
-function File(fileID, fileName, filePath) {
-  this.fileID = fileID;
-  this.fileName = fileName;
-  this.filePath = filePath;
+
+
+export default function File(fileID, fileName, filePath, parentFolder) {
+  this.id = fileID;
+  this.name = fileName;
+  this.path = filePath;
+  this.parentFolder = parentFolder;
   console.log('new File created');
 }
 
-module.exports = File;
+/**
+ * Remove file from the database
+ *
+ * @param {}
+ * @return {}
+ */
+File.prototype.remove = function remove() {
+  console.log('file.remove');
+  // Case 1a: file exist, file removed
+  // Case 1b: file exist, file not removed (error)
+  // Case 2: file does not exist (nothing removed)
+
+  // storage.deleteFile(this.path);
+  const parentFolder = this.parentFolder;
+  const index = parentFolder.childFileList.findIndex((childFile) => {
+    return childFile.id === this.id;
+  });
+  parentFolder.childFileList.splice(index, 1);
+};
+
+/**
+ * Save data into file
+ *
+ * @param {String} data
+ * @return {}
+ */
+File.prototype.save = function save(data) {
+  console.log('file.save');
+  // Case 1a: file exist, file saved
+  // Case 1b: file exist, file not saved (error)
+  // Case 2: file does not exist (nothing saved)
+
+  // return storage_api.saveFile(this.id, data);
+};
+
+/**
+ * Load data from file
+ *
+ * @param {}
+ * @return {String} data loaded from file
+ */
+File.prototype.load = function load() {
+  // Case 1a: file exist, file loaded
+  // Case 1b: file exist, file not loaded (error)
+  // Case 2: file does not exist (nothing loaded)
+
+  // return storage_api.loadFile(this.id);
+};
+
+/**
+ * Copy file
+ *
+ * @param {Folder} folder default: currentFolder
+ * @return {}
+ */
+File.prototype.copy = function copy(folder) {
+  console.log('file.copy');
+  // Case 1a: file exist, valid folder, file copied
+  // Case 1b: file exist, invalid folder, file not copied (error)
+  // Case 2: file does not exist (not copied)
+  // Case 3: file exist, folder not specified, default: current folder used
+};
+
+/**
+ * Move file to a specified folder
+ *
+ * @param {Folder} folder
+ * @return {}
+ */
+File.prototype.move = function move() {
+  console.log('file.move');
+  // Changing file path
+  // Case 1a: file exist, valid folder, file moved
+  // Case 1b: file exist, invalid folder, file not moved (error)
+  // Case 1c: file exist, folder is where file currently reside in (not moved)
+  // Case 2: file does not exist (not moved)
+};
+
+/**
+ * Download file from database to drive
+ *
+ * @param {String} fileFormat deafult: md format
+ * @return {}
+ */
+File.prototype.download = function download() {
+  console.log('file.download');
+};
+
+/**
+ * Upload file from drive to database
+ *
+ * @param {}
+ * @return {}
+ */
+File.prototype.upload = function upload() {
+  console.log('file.upload');
+};
+
+/**
+ * Rename file
+ *
+ * @param {String} newFileName
+ * @return {}
+ */
+File.prototype.rename = function rename(newFileName) {
+  console.log('file.rename');
+  this.fileName = newFileName;
+  // update database
+};
