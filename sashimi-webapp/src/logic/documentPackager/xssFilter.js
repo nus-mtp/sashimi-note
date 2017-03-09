279482a168d4e4884b3c5991a1b74de40ae22293
JxJx <justjx@hotmail.com>
2017-03-09 13:26:41 +0800

    Update xssFilter and test spec fail error

--- sashimi-webapp/src/logic/documentPackager/xssFilter.js
+++ sashimi-webapp/src/logic/documentPackager/xssFilter.js
@@ -8,6 +8,9 @@
 */
 import xssFilter from 'xss';
 
+// some standard attributes that will be allowed
+const allowedAttr = ['id', 'class', 'style'];
+
 // get a copy of default whiteList
 const whiteList = xssFilter.getDefaultWhiteList();
 
@@ -18,18 +21,10 @@ const whiteList = xssFilter.getDefaultWhiteList();
 ** Will require reviewing/reconstructing after proper integration as
 ** this is too "ugly"
 */
-whiteList.pre.push('class');
-whiteList.code.push('class');
-whiteList.span.push('class', 'aria-hidden', 'style', 'role');
-whiteList.h1.push('id');
-whiteList.h2.push('id');
-whiteList.h3.push('id');
-whiteList.h4.push('id');
-whiteList.h5.push('id');
-whiteList.h6.push('id');
-whiteList.div.push('class');
+whiteList.span.push('aria-hidden', 'role');
+whiteList.br.push('page');
 
-// New tags not in whiteList that are required by plugins
+// New tags not in whiteList that are required by KaTeX plugin
 whiteList.annotation = ['encoding'];
 whiteList.math = [];
 whiteList.semantics = [];
@@ -39,44 +34,53 @@ whiteList.mn = [];
 whiteList.mi = [];
 whiteList.mo = [];
 whiteList.msup = [];
-whiteList.pagebreak = [];
+// allow ol specify start number
+whiteList.ol = ['start'];
+// allow li specify value number
+whiteList.li = ['value'];
+// allow style tag
+whiteList.style = [];
+// allow kbd tag
+whiteList.kbd = [];
+// allow ifram tag with some safe attributes
+whiteList.iframe = ['allowfullscreen', 'name', 'referrerpolicy', 'sandbox', 'src', 'srcdoc', 'width', 'height'];
+// allow summary tag
+whiteList.summary = [];
 
 // Custom safeAttrValue function for whiteList
-function safeAttrValue(tag, name, value, cssFilter) {
-  // only when the tag is 'span' and attribute is 'style'
-  // then use your custom function
-  if (tag === 'span' && name === 'style') {
-    // only filter the value that start with 'javascript:'
-    if (/^javascript:/ig.test(value)) {
-      return '';
-    }
-    return value;
-  } else if (tag === 'div' && name === 'class') {
-    if (/^javascript:/ig.test(value)) {
-      return '';
-    }
-    return value;
-  } else if (tag === 'h1' && name === 'id') {
-    if (/^javascript:/ig.test(value)) {
-      return '';
-    }
-    return value;
+function safeAttrValue(tag, name, value) {
+  // any value that is 'javascript:'
+  // it will be filtered to ''
+  if (/^javascript:/ig.test(value)) {
+    return '';
   } else {
     // use the default safeAttrValue function to process it
-    return xssFilter.safeAttrValue(tag, name, value, cssFilter);
+    return xssFilter.safeAttrValue(tag, name, value);
+  }
+}
+
+function onIgnoreTagAttr(tag, name, value, isWhiteAttr) {
+  // allow attr in the whiteListAttr
+  if (allowedAttr.indexOf(name) !== -1) {
+    // escape its value using custom safeAttrValue function
+    return `${name}="${safeAttrValue(tag, name, value)}"`;
+  } else {
+    // for attributes that are not whitelisted
+    // use default onIgnoreTagAttr function to process it
+    return xssFilter.onIgnoreTagAttr(tag, name, value, isWhiteAttr);
   }
 }
 
 // Setting xssFilter to use customised whiteList
 const options = {
   whiteList,
-  safeAttrValue
+  safeAttrValue,
+  onIgnoreTagAttr
 };
 
 export default {
   filter: function format(data) {
     // Filter out any possible XSS threat
-    // console.log(whiteList);
     data = xssFilter(data, options);
 
     // Return data as it is for now.
