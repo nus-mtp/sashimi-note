6e9b283562d2b755611aed3de0ba5a36ce879726
amoshydra <amoswwj@gmail.com>
2017-03-09 09:14:28 +0800

    Extract element compute height function into helper module

--- sashimi-webapp/src/logic/renderer/helper.js
+++ sashimi-webapp/src/logic/renderer/helper.js
@@ -22,6 +22,29 @@ export default {
          );
   },
 
+  computeElementHeight(element) {
+    const nodeStyle = this.getComputedStyle(element);
+
+    // Get node's height
+    const nodeStyleHeight = parseFloat(nodeStyle.height, 10) || 0;
+    const nodeHeight = Math.max(
+      element.clientHeight,
+      element.offsetHeight,
+      nodeStyleHeight
+    );
+
+    // Get node's margin
+    const nodeMargin = parseFloat(nodeStyle.marginTop, 10) +
+                      parseFloat(nodeStyle.marginBottom, 10);
+
+    const totalHeight = nodeHeight + nodeMargin;
+    if (isNaN(totalHeight)) {
+      throw new Error('Error calculating element\'s height');
+    }
+
+    return totalHeight;
+  },
+
   /**
    * Get computed style of an element.
    * @param {Element} element
