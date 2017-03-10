c56dfae4ce5996ca727c15ebbcee47028c8e7521
amoshydra <amoswwj@gmail.com>
2017-03-10 12:33:56 +0800

    Add special eslint exception for e2e specs file
    for better code readability

---
+++ sashimi-webapp/test/e2e/.eslintrc.js
@@ -0,0 +1,10 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  // add your custom rules here
+  'rules': {
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "func-names": "off",
+  }
+}
