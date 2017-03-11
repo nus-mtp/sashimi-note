8107a9ea080b126ab687ddcb9282efd2aef5b817
amoshydra <amoswwj@gmail.com>
2017-03-11 13:05:06 +0800

    Change nightwatch to use mocha

--- sashimi-webapp/test/e2e/specs/pageRenderer/oversized-content-at-middle.spec.js
+++ sashimi-webapp/test/e2e/specs/pageRenderer/oversized-content-at-middle.spec.js
@@ -1,23 +1,24 @@
 const textLoader = require('../../helpers/textLoader');
 const pagesModeActivation = require('./pages-mode-activation');
 
-module.exports = {
-  'should activate Pages mode': pagesModeActivation['should activate Pages mode'],
-
-  'should render oversized content in the next page if it is not the first item in the page': (browser) => {
+describe('Viewer\'s manage file button', function() {
+  it('should render oversized content in the next page if it is not the first item in the page', (browser) => {
+    pagesModeActivation(browser);
     browser
       .execute((data) => {
         const codeMirrorInstance = document.getElementsByClassName('CodeMirror')[0].CodeMirror;
         codeMirrorInstance.setValue(data);
       }, [textLoader.load('references/oversized-content-at-middle')], () => {
         browser
-          .waitForElementPresent('.page-view:nth-child(4)', 5000, '4 pages are rendered')
-          .waitForElementPresent('.page-view:nth-child(3) pre code', 5000, 'Code block is rendered in page 3');
+          .expect.element('.page-view:nth-child(4)')
+          .to.be.present.before(5000);
+        browser
+          .expect.element('.page-view:nth-child(3) pre code')
+          .to.be.present.before(5000);
       });
-  },
+  });
 
-  'after': (browser) => {
-    browser.end();
-  },
-
-};
+  after((browser, done) => {
+    browser.end(() => done());
+  });
+});
--- sashimi-webapp/test/e2e/specs/pageRenderer/pages-mode-activation.js
+++ sashimi-webapp/test/e2e/specs/pageRenderer/pages-mode-activation.js
@@ -1,24 +1,29 @@
-module.exports = {
-  'should activate Pages mode': (browser) => {
-    const devServer = browser.globals.devServerURL;
+const activatePagesMode = function activatePagesMode(browser) {
+  const devServer = browser.globals.devServerURL;
 
-    browser.url(`${devServer}/content`);
-    browser.expect.element('#app').to.be.visible.before(5000);
+  browser.url(`${devServer}/content`);
+  browser.expect.element('#app').to.be.visible.before(5000);
 
-    browser.expect.element('.viewer').to.be.present;
-    browser.expect.element('#viewer-container').to.be.present;
-    browser.expect.element('#manage-file').to.be.present;
+  browser.expect.element('.viewer').to.be.present;
+  browser.expect.element('#viewer-container').to.be.present;
+  browser.expect.element('#manage-file').to.be.present;
 
-    browser
-      .click('#manage-file')
-      .pause(500)
-      .click('button[data-format="pages"]')
-      .pause(500);
-    browser.expect.element('.viewer[data-fileformat="pages"]').to.be.present.before(1000);
-  },
-
-  'after': (browser) => {
-    browser.end();
-  },
-
+  browser
+    .click('#manage-file')
+    .pause(500)
+    .click('button[data-format="pages"]')
+    .pause(500);
+  browser.expect.element('.viewer[data-fileformat="pages"]').to.be.present.before(1000);
 };
+
+describe('Viewer\'s manage file button', function() {
+  after((browser, done) => {
+    browser.end(() => done());
+  });
+
+  it('should activate Pages mode', (browser) => {
+    activatePagesMode(browser);
+  });
+});
+
+module.exports = activatePagesMode;
