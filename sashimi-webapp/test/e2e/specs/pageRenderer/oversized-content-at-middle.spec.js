8dbd663a637b9c632f75dc77ecc592486beaecef
amoshydra <amoswwj@gmail.com>
2017-03-11 13:29:38 +0800

    Add async done callback for page mode activation

--- sashimi-webapp/test/e2e/specs/pageRenderer/oversized-content-at-middle.spec.js
+++ sashimi-webapp/test/e2e/specs/pageRenderer/oversized-content-at-middle.spec.js
@@ -1,9 +1,12 @@
 const textLoader = require('../../helpers/textLoader');
 const pagesModeActivation = require('./pages-mode-activation');
 
-describe('Viewer\'s manage file button', function() {
-  it('should render oversized content in the next page if it is not the first item in the page', (browser) => {
-    pagesModeActivation(browser);
+describe('Render oversized content in the next page if it is not the first item in the page', function() {
+  before((browser, done) => {
+    pagesModeActivation(browser, done);
+  });
+
+  it('should attempt to insert code block into page 2 and render it at page 3', (browser) => {
     browser
       .execute((data) => {
         const codeMirrorInstance = document.getElementsByClassName('CodeMirror')[0].CodeMirror;
--- sashimi-webapp/test/e2e/specs/pageRenderer/pages-mode-activation.js
+++ sashimi-webapp/test/e2e/specs/pageRenderer/pages-mode-activation.js
@@ -1,4 +1,4 @@
-const activatePagesMode = function activatePagesMode(browser) {
+const activatePagesMode = function activatePagesMode(browser, callback) {
   const devServer = browser.globals.devServerURL;
 
   browser.url(`${devServer}/content`);
@@ -14,6 +14,8 @@ const activatePagesMode = function activatePagesMode(browser) {
     .click('button[data-format="pages"]')
     .pause(500);
   browser.expect.element('.viewer[data-fileformat="pages"]').to.be.present.before(1000);
+
+  if (typeof callback === 'function') browser.perform(() => callback());
 };
 
 describe('Viewer\'s manage file button', function() {
