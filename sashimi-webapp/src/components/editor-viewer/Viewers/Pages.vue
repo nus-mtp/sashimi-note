5798711d5000db8b6af67adb7f56277d1b2f21f8
Amos Wong <amoswwj@gmail.com>
2017-03-12 10:16:21 +0800

    Refactor codes in slides and pages mode

--- sashimi-webapp/src/components/editor-viewer/Viewers/Pages.vue
+++ sashimi-webapp/src/components/editor-viewer/Viewers/Pages.vue
@@ -7,35 +7,28 @@
   import _ from 'lodash';
   import PageRenderer from 'src/logic/renderer';
   
-  // Declare page renderers instance. These renderer
-  // will be initialised when this component is mounted.
-  let pageRenderer = null;
-
   // Throttle function used to limit the rate which
   // the render function is called
   const throttleTime = 600;
   const renderThrottleFn = _.throttle((htmlData, pr) => pr.write(htmlData), throttleTime);
 
-  const updateViewer = ((component) => {
-    renderThrottleFn(component.htmlData, pageRenderer);
-  });
-
   export default {
     props: ['htmlData'],
     data() {
-      return {};
+      return {
+        pageRenderer: null
+      };
     },
     watch: {
       htmlData() {
-        updateViewer(this);
+        renderThrottleFn(this.htmlData, this.pageRenderer);
       }
     },
     mounted() {
       // Mount does not gurrantee DOM to be ready, thus nextTick is used
-      const self = this;
       Vue.nextTick(() => {
-        pageRenderer = new PageRenderer('viewer-container');
-        updateViewer(self);
+        this.pageRenderer = new PageRenderer('viewer-container');
+        renderThrottleFn(this.htmlData, this.pageRenderer);
       });
     }
   };
--- sashimi-webapp/src/components/editor-viewer/Viewers/Slides.vue
+++ sashimi-webapp/src/components/editor-viewer/Viewers/Slides.vue
@@ -7,27 +7,21 @@
   import _ from 'lodash';
   import PageRenderer from 'src/logic/renderer';
   
-  // Declare page renderers instance. These renderer
-  // will be initialised when this component is mounted.
-  let pageRenderer = null;
-
   // Throttle function used to limit the rate which
   // the render function is called
   const throttleTime = 600;
   const renderThrottleFn = _.throttle((htmlData, pr) => pr.write(htmlData), throttleTime);
 
-  const updateViewer = ((component) => {
-    renderThrottleFn(component.htmlData, pageRenderer);
-  });
-
   export default {
     props: ['htmlData'],
     data() {
-      return {};
+      return {
+        pageRenderer: null
+      };
     },
     watch: {
       htmlData() {
-        updateViewer(this);
+        renderThrottleFn(this.htmlData, this.pageRenderer);
       }
     },
     mounted() {
@@ -43,10 +37,9 @@
       };
 
       // Mount does not gurrantee DOM to be ready, thus nextTick is used
-      const self = this;
       Vue.nextTick(() => {
-        pageRenderer = new PageRenderer('viewer-container', PAGE_A6);
-        updateViewer(self);
+        this.pageRenderer = new PageRenderer('viewer-container', PAGE_A6);
+        renderThrottleFn(this.htmlData, this.pageRenderer);
       });
     }
   };
