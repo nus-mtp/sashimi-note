c0057785ec4c1ca0ef03839071be5104dad5967b
jiayingy <jiayinglek@gmail.com>
2017-03-12 19:13:12 +0800

    Attach 'changeViewMode' and 'execute' event listener to userInput element

--- sashimi-webapp/src/components/file-manager/FileManager.vue
+++ sashimi-webapp/src/components/file-manager/FileManager.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <userInputs v-model="action"></userInputs>
-    <documents :action="action"></documents>
+    <userInputs 
+      v-on:changeViewMode="changeViewMode"
+      v-on:execute="executeAction"
+    ></userInputs>
+    <documents 
+      :viewMode="viewMode"
+    ></documents>
   </div>
 </template>
 
 <script>
+// import fileManager from 'src/logic/filemanager';
 import documents from './Documents';
 import userInputs from './UserInputs';
+
+// const mockFolder = {
+//   id: 0,
+//   name: 'root',
+//   path: '/',
+//   parentFolder: null,
+//   childFolderList: [], // list of folders in current folder
+//   childFileList: [], // list of files in current folder
+// };
 
 export default {
   components: {
@@ -16,11 +31,30 @@ export default {
   },
   data() {
     return {
-      action: '',
+      viewMode: 'listView',
     };
   },
   watch: {
+  },
+  methods: {
+    changeViewMode(viewMode) {
+      this.viewMode = viewMode;
+    },
+    executeAction(action) {
+      this.$emit('executeAction', action);
+    }
   }
+  // mounted() {
+  //   const startPromise = fileManager.start();
+
+  //   startPromise
+  //   .then((rootFolder) => {
+  //     console.log(rootFolder, 'rootFolder');
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // }
 };
 
 </script>
