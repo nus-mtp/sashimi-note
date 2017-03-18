9b8aafbe7adf65d5b7dfeba4985895f7e7db806d
jiayingy <jiayinglek@gmail.com>
2017-03-19 01:00:26 +0800

    Check for existence of navbar components in unit test

--- sashimi-webapp/test/unit/specs/Content.spec.js
+++ sashimi-webapp/test/unit/specs/Content.spec.js
@@ -11,7 +11,42 @@ describe('Content.vue', () => {
   // A mock viewer-container DOM is needed for this test.
   // It will be created before all the test are ran, and remove
   // when all the tests are finished.
-
+  it('should render Navbar component', () => {
+    const Constructor = Vue.extend(Content);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.navbar'))
+      .to.not.equal(null);
+  });
+  it('should render toggle edit button', () => {
+    const Constructor = Vue.extend(Content);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.navbar #button-editor'))
+      .to.not.equal(null);
+  });
+  it('should render toggle edit button', () => {
+    const Constructor = Vue.extend(Content);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.navbar #button-viewer'))
+      .to.not.equal(null);
+  });
+  it('should render toggle edit button', () => {
+    const Constructor = Vue.extend(Content);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.navbar #button-split-screen'))
+      .to.not.equal(null);
+  });
+  it('should render Editor wrapper', () => {
+    const Constructor = Vue.extend(Content);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.editor-wrapper'))
+      .to.not.equal(null);
+  });
+  it('should render Viewer wrapper', () => {
+    const Constructor = Vue.extend(Content);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.viewer-wrapper'))
+      .to.not.equal(null);
+  });
   it('should render Editor component', () => {
     const Constructor = Vue.extend(Content);
     const vm = new Constructor().$mount();
