1216e5053f15382bb8d5535ae2eefc31b986c3da
tharain <won.jr@hotmail.com>
2017-03-18 23:17:27 +0800

    Add space for aesthetic of array to sqlArray.spec.js

--- sashimi-webapp/test/unit/specs/database/generated-data/sqlArray.spec.js
+++ sashimi-webapp/test/unit/specs/database/generated-data/sqlArray.spec.js
@@ -18,7 +18,7 @@ describe('sqlArray.js', () => {
       alasqlArray.addKeyBasePair('a', 123);
       alasqlArray.addKeyBasePair('b', '123');
       const alaArray = alasqlArray.endAlasqlArray();
-      expect(alaArray).to.deep.equal([{a: 123, b: '123'}]);
+      expect(alaArray).to.deep.equal([{ a: 123, b: '123' }]);
       expect(typeof alaArray).to.equal('object');
       expect(typeof alaArray[0]).to.equal('object');
       expect(typeof Object.values(alaArray[0])).to.equal.null;
