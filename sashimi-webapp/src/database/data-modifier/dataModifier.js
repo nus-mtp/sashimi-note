a625af12d8038b20fa406b8290dc0c95a81dd4eb
tharain <won.jr@hotmail.com>
2017-03-19 01:08:01 +0800

    Update dataUpdate functions from dataModifier.js
    Shift changeFilePath, changeFileName and changeFileName implementation
    to dataUpdate.js to better suit the dataModifier as a facade class

--- sashimi-webapp/src/database/data-modifier/dataModifier.js
+++ sashimi-webapp/src/database/data-modifier/dataModifier.js
@@ -7,16 +7,6 @@
 import dataAdd from 'src/database/data-modifier/dataAdd';
 import dataDelete from 'src/database/data-modifier/dataDelete';
 import dataUpdate from 'src/database/data-modifier/dataUpdate';
-import SqlCommands from 'src/database/sql-related/sqlCommands';
-import StringManipulator from 'src/database/stringManipulation';
-
-const stringManipulator = new StringManipulator();
-
-const sqlCommands = new SqlCommands();
-
-function resolveFileSaving(fileContent) {
-  return stringManipulator.replaceAll(fileContent, '"', '\\"');
-}
 
 export default class dataModifier {
   static constructor() {}
@@ -34,19 +24,18 @@ export default class dataModifier {
   }
 
   static moveFile(fileId, newPath) {
-    return sqlCommands.changeFilePath(fileId, newPath);
+    return dataUpdate.changeFilePath(fileId, newPath);
   }
 
   static renameFileName(fileId, newFileName) {
-    return sqlCommands.changeFileName(fileId, newFileName);
+    return dataUpdate.changeFileName(fileId, newFileName);
   }
 
   static renameFolderName(folderId, newFolderName) {
-    return sqlCommands.changeFolderName(folderId, newFolderName);
+    return dataUpdate.changeFolderName(folderId, newFolderName);
   }
 
   static saveFile(fileId, fileContent) {
-    fileContent = resolveFileSaving(fileContent);
     return dataUpdate.saveFile(fileId, fileContent);
   }
 
--- sashimi-webapp/src/database/data-modifier/dataUpdate.js
+++ sashimi-webapp/src/database/data-modifier/dataUpdate.js
@@ -1,11 +1,30 @@
 import SqlCommands from 'src/database/sql-related/sqlCommands';
+import StringManipulator from 'src/database/stringManipulation';
 
 const sqlCommands = new SqlCommands();
+const stringManipulator = new StringManipulator();
+
+function resolveFileSaving(fileContent) {
+  return stringManipulator.replaceAll(fileContent, '"', '\\"');
+}
 
 export default class dataUpdate {
   static constructor() {}
 
   static saveFile(fileId, markdownFile) {
+    markdownFile = resolveFileSaving(markdownFile);
     return sqlCommands.saveFile(fileId, markdownFile);
+  }
+
+  static changeFilePath(fileId, newPath) {
+    return sqlCommands.changeFilePath(fileId, newPath);
+  }
+
+  static changeFileName(fileId, newFileName) {
+    return sqlCommands.changeFileName(fileId, newFileName);
+  }
+
+  static changeFolderName(folderId, newFolderName) {
+    return sqlCommands.changeFolderName(folderId, newFolderName);
   }
 }
