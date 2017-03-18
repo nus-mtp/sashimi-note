087af069284650d64390235f76bb40508acb768d
tharain <won.jr@hotmail.com>
2017-03-18 13:54:46 +0800

    Added sqlArray.spec.js
    [1] test creation of an empty object
    [2] test fill up an object of object

---
+++ sashimi-webapp/test/unit/specs/database/generated-data/sqlArray.spec.js
@@ -0,0 +1,31 @@
+import SqlArray from 'src/database/generated-data/sqlArray';
+
+const alasqlArray = new SqlArray();
+
+describe('sqlArray.js', () => {
+  describe('creation of datastructure for alasql', () => {
+    it('should create an empty object of object', () => {
+      alasqlArray.initializeAlasqlArray();
+      const alaArray = alasqlArray.endAlasqlArray();
+      expect(alaArray).to.deep.equal([{}]);
+      expect(typeof alaArray).to.equal('object');
+      expect(typeof alaArray[0]).to.equal('object');
+      /* eslint-disable */
+      expect(typeof Object.values(alaArray[0])).to.equal.null;
+      /* esline-enable */
+    });
+
+    it('should fill up an object of object', () => {
+      alasqlArray.initializeAlasqlArray();
+      alasqlArray.addKeyBasePair('a', 123)
+      alasqlArray.addKeyBasePair('b', '123')
+      const alaArray = alasqlArray.endAlasqlArray();
+      expect(alaArray).to.deep.equal([{a: 123, b: '123'}]);
+      expect(typeof alaArray).to.equal('object');
+      expect(typeof alaArray[0]).to.equal('object');
+      /* eslint-disable */
+      expect(typeof Object.values(alaArray[0])).to.equal.null;
+      /* esline-enable */
+    });
+  });
+});
