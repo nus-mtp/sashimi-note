b93c0845188cbc640bd5294ac2d703869d8aadb6
JxJx <justjx@hotmail.com>
2017-03-19 12:26:08 +0800

    Update karma.conf edited by Amos

--- sashimi-webapp/test/unit/karma.conf.js
+++ sashimi-webapp/test/unit/karma.conf.js
@@ -3,9 +3,9 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-var webpackConfig = require('../../build/webpack.test.conf');
+const webpackConfig = require('../../build/webpack.test.conf');
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
@@ -14,7 +14,10 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: [
+      { pattern: '../../static/vendors/*.js', included: true, served: true },
+      './index.js',
+    ],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
