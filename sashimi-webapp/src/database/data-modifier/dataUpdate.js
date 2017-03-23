718cca1dbff2b9060fdd0a612398271ed4d9838a
tharain <won.jr@hotmail.com>
2017-03-23 17:16:27 +0800

    Remove handling of quotes in facade class dataUpdate.js

--- sashimi-webapp/src/database/data-modifier/dataUpdate.js
+++ sashimi-webapp/src/database/data-modifier/dataUpdate.js
@@ -1,18 +1,11 @@
 import SqlCommands from 'src/database/sql-related/sqlCommands';
-import StringManipulator from 'src/database/stringManipulation';
 
 const sqlCommands = new SqlCommands();
-const stringManipulator = new StringManipulator();
-
-function resolveFileSaving(fileContent) {
-  return stringManipulator.replaceAll(fileContent, '"', '\\"');
-}
 
 export default class dataUpdate {
   static constructor() {}
 
   static saveFile(fileId, markdownFile) {
-    markdownFile = resolveFileSaving(markdownFile);
     return sqlCommands.saveFile(fileId, markdownFile);
   }
 
