8e2593a91febdb77b11ac92cac16d5f6fb11549f
Amos Wong <amoswwj@gmail.com>
2017-03-26 06:00:08 +0800

    Experiment with iframe as render target for HTML.vue

--- sashimi-webapp/src/components/editor-viewer/Viewers/Html.vue
+++ sashimi-webapp/src/components/editor-viewer/Viewers/Html.vue
@@ -1,29 +1,55 @@
 <template>
-  <div id="viewer-container"></div>
+  <iframe 
+    id="viewer-container"
+    width="100%"
+    height="100%"
+  ></iframe>
 </template>
 
 <script>
+  import Vue from 'vue';
   import diagramsRenderer from 'src/logic/renderer/diagrams';
 
   /**
    * Diagram rendering function for HTML view
+   * @param {element} renderTarget - HTML element that will be used to render data into
    * @param {string} htmlData - string containing the parsed and rendered markdown syntax by markdown-it
    */
-  function renderUpdate(htmlData) {
-    document.getElementById('viewer-container').innerHTML = htmlData;
+  function renderUpdate(renderTarget, htmlData) {
+    renderTarget.innerHTML = htmlData;
     // find everything and replace/drawsvg
-    diagramsRenderer(document.getElementById('viewer-container'));
+    diagramsRenderer(renderTarget);
   }
 
   export default {
     props: ['htmlData'],
+    data() {
+      return {
+        renderDoc: null,
+      };
+    },
     watch: {
-      htmlData() {
-        renderUpdate(this.htmlData);
+      htmlData(data) {
+        renderUpdate(this.renderDoc.body, data);
       }
     },
     mounted() {
-      renderUpdate(this.htmlData);
+      Vue.nextTick(() => {
+        this.renderDoc = this.$el.contentWindow.document;
+
+        const styling = this.renderDoc.createElement('link');
+        const attributes = {
+          type: 'text/css',
+          rel: 'stylesheet',
+          href: '/styles/markdown-html.css'
+        };
+
+        Object.keys(attributes).forEach((key) => {
+          styling.setAttribute(key, attributes[key]);
+        });
+        this.renderDoc.head.appendChild(styling);
+        renderUpdate(this.renderDoc.body, this.htmlData);
+      });
     }
   };
 
