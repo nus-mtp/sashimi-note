d6b800045569cf092fcb9a7dbd1df4706b5bcec7
amoshydra <amoswwj@gmail.com>
2017-04-09 04:25:13 +0800

    Make iframeBuilder addStyle to be compatible with more browser

--- sashimi-webapp/src/helpers/iframeBuilder.js
+++ sashimi-webapp/src/helpers/iframeBuilder.js
@@ -43,36 +43,64 @@ export default {
    * @param {Element} iframeElement
    * @param {string} url string pointing to a stylesSheet
    * @return {Promise} return a promise when all the styles has been loaded
+   * @throws {Error} "Error loading style link..." if link failed to load or is empty.
    */
-  addStyle(iframeElement, style) {
+  addStyle(iframeElement, styleUrl) {
     return new Promise((resolve, reject) => {
-      const styleElement = constructStyleLink(iframeElement.contentWindow.document, style);
-      this.getDocument(iframeElement).head.appendChild(styleElement);
+      const styleElement = constructStyleLink(iframeElement.contentWindow.document, styleUrl);
 
       // Resolve only when the style has been loaded.
+      // Monitoring is done before stylesheet got inserted.
       // https://www.phpied.com/when-is-a-stylesheet-really-loaded/
-      // This doesn't work well with Firefox and IE. This will result in
-      //   the initial rendering of css to not work correctly.
-      //   However, subsequent rendering should work normally.
-      // Temporary solution:
-      //   All CSS will get loaded in 10ms, a polling of 50ms setInterval
-      //   is sufficient to ensure all css get loaded and rendered.
-      // TODO: Implement a better solution for this problem
-      const poller = setInterval(() => {
-        const styleSheets = iframeElement.contentWindow.document.styleSheets;
-
-        for (let i = 0; i < styleSheets.length; i += 1) {
-          const stylesheet = styleSheets[i];
-
-          // A trival way to check if the intended css has been loaded.
-          // This method check for substring only, so stylesheet with similar
-          //   name will not work.
-          if (stylesheet.href.includes(style)) {
-            resolve();
+      if (styleElement.onload === null) { // onload is defined but not set
+        // Solution for most modern browser.
+        // 'onload' will be emitted by all major browser,
+        //   IE/Edge, Webkit and Chrome > 2012, Firefox > 2014.
+        styleElement.onload = () => {
+          resolve();
+        };
+        styleElement.onerror = (event) => {
+          reject(new Error(`Error loading style link: ${event.target.href}`));
+        };
+      } else {
+        // Fall back solution for older browser before 2014
+        // This solution only for for Chrome, Safari and Webkit browser
+        const pollIncrement = 50;
+        const poller = setInterval(() => {
+          // If the iframe window doesn't exist, loading of style should be terminated.
+          //   Since the content no longer exist. There is no need to load the style anymore.
+          //   Therefore, no error should be thrown.
+          if (!iframeElement.contentWindow) {
             clearInterval(poller);
+            return;
           }
-        }
-      }, 50);
+
+          const styleSheets = iframeElement.contentWindow.document.styleSheets;
+          for (let i = 0; i < styleSheets.length; i += 1) {
+            const stylesheet = styleSheets[i];
+
+            // A trival way to check if the intended css has been loaded.
+            // This method check for substring only, so stylesheet with similar
+            //   name will not work.
+            const hasParsedStyleSheet = stylesheet.href.includes(styleUrl);
+
+            if (hasParsedStyleSheet) {
+              clearInterval(poller);
+              const styleRules = stylesheet.rule || stylesheet.cssRules;
+              if (styleRules.length > 0) {
+                resolve();
+              } else {
+                // CSS style is either empty or failed to load.
+                reject(`Error loading style link: ${stylesheet.href}`);
+              }
+              return;
+            }
+          }
+        }, pollIncrement);
+      }
+
+      // Insert style link into stylesheet
+      this.getDocument(iframeElement).head.appendChild(styleElement);
     });
   },
 
@@ -81,10 +109,11 @@ export default {
    * @param {Element} iframeElement
    * @param {Array<string>} An array of url string pointing to a stylesSheet
    * @return {Promise} return a promise when all the styles has been loaded
+   * @throws {Error} "Error loading style link..." if link failed to load or is empty.
    */
-  addStyles(iframeElement, styles) {
-    return Promise.all(styles.map(style =>
-      this.addStyle(iframeElement, style)
+  addStyles(iframeElement, stylesUrl) {
+    return Promise.all(stylesUrl.map(styleUrl =>
+      this.addStyle(iframeElement, styleUrl)
     ));
   },
 };
