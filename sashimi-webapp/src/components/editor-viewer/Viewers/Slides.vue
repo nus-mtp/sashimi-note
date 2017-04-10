e7dfaed1c607049a03a32f033f212a8842cea7ba
amoshydra <amoswwj@gmail.com>
2017-04-10 16:03:41 +0800

    Rename documentBuilder dependency

--- sashimi-webapp/src/components/editor-viewer/Viewers/Slides.vue
+++ sashimi-webapp/src/components/editor-viewer/Viewers/Slides.vue
@@ -12,7 +12,7 @@
   import _ from 'lodash';
   import PageRenderer from 'src/logic/renderer';
   import DocumentNavigator from 'src/logic/inputHandler/DocumentNavigator';
-  import iframeBuilder from 'src/helpers/iframeBuilder';
+  import documentBuilder from 'src/helpers/documentBuilder';
 
   // Throttle function used to limit the rate which
   // the render function is called
@@ -46,14 +46,14 @@
 
       // Mount does not gurrantee DOM to be ready, thus nextTick is used
       Vue.nextTick(() => {
-        iframeBuilder.rebuild(this.$el);
-        iframeBuilder.addStyles(this.$el, [
+        documentBuilder.rebuild(this.$el);
+        documentBuilder.addStyles(this.$el, [
           '/styles/markdown-html.css',
           '/styles/viewer-page.css',
           '/styles/markdown-imports.css'
         ])
         .then(() => {
-          const iframeDoc = iframeBuilder.getDocument(this.$el);
+          const iframeDoc = documentBuilder.getDocument(this.$el);
 
           const eleParent = iframeDoc.createElement('div');
           const eleContainer = iframeDoc.createElement('div');
