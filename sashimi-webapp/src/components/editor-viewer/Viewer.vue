5f36e94637f7be6d563288801ca7323a0f0b888b
amoshydra <amoswwj@gmail.com>
2017-04-10 16:04:45 +0800

    Reimplement the print mechanism with javascript

--- sashimi-webapp/src/components/editor-viewer/Viewer.vue
+++ sashimi-webapp/src/components/editor-viewer/Viewer.vue
@@ -19,6 +19,8 @@
   import Vue from 'vue';
   import AsyncComputed from 'vue-async-computed';
   import documentPackager from 'src/logic/documentPackager';
+  import documentBuilder from 'src/helpers/documentBuilder';
+  import PageRenderer from 'src/logic/renderer';
   import viewerPages from './Viewers/Pages';
   import viewerSlides from './Viewers/Slides';
   import viewerHtml from './Viewers/Html';
@@ -37,6 +39,51 @@
         return documentPackager.getHtmlData(this.editorContent);
       }
     },
+    mounted() {
+      // Print
+      this.printDocument = (event) => {
+        if (event.key === 'p' && event.ctrlKey) {
+          event.preventDefault();
+
+          const frame = {
+            url: 'about:blank',
+            target: '_blank',
+            feature: 'location=yes,height=600,width=800,scrollbars=yes',
+          };
+
+          const printFrame = window.open(frame.url, frame.target, frame.feature);
+          documentBuilder.rebuild(printFrame);
+          documentBuilder.addStyles(printFrame, [
+            '/styles/markdown-html.css',
+            '/styles/viewer-page.css',
+            '/styles/markdown-imports.css'
+          ])
+          .then(() => {
+            const iframeDoc = documentBuilder.getDocument(printFrame);
+            iframeDoc.title = 'Sashimi Printed Document';
+
+            const eleParent = iframeDoc.createElement('div');
+            const eleContainer = iframeDoc.createElement('div');
+            eleParent.appendChild(eleContainer);
+            iframeDoc.body.appendChild(eleParent);
+            return eleContainer;
+          })
+          .then((renderTarget) => {
+            const pr = new PageRenderer(renderTarget);
+            return documentPackager.getHtmlData(this.editorContent)
+            .then(htmlData => pr.write(htmlData));
+          })
+          .then(() => {
+            printFrame.focus();
+            printFrame.print();
+          });
+        }
+      };
+      window.addEventListener('keydown', this.printDocument);
+    },
+    beforeDestroy() {
+      window.removeEventListener('keydown', this.printDocument);
+    }
   };
 
 </script>
