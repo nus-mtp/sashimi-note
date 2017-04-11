cd166f0bd0df4d5f7f21b2eeec833164d8024a92
Amos Wong <amoswwj@gmail.com>
2017-04-11 19:41:53 +0800

    Fix typo

--- sashimi-webapp/src/components/editor-viewer/Viewer.vue
+++ sashimi-webapp/src/components/editor-viewer/Viewer.vue
@@ -58,7 +58,7 @@
       Vue.nextTick(() => {
         documentPrinter = new DocumentPrinter(window, this, 'editorContent');
       });
-    }
+    },
     beforeDestroy() {
       documentPrinter.unsetDomBehaviour();
     }
