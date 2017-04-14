4a84d4f3b1e71740cffa27d240e34ef058996d43
Amos Wong <amoswwj@gmail.com>
2017-04-15 05:13:33 +0800

    Fix element utils documentation

--- sashimi-webapp/src/helpers/elementUtils.js
+++ sashimi-webapp/src/helpers/elementUtils.js
@@ -1,6 +1,6 @@
 export default {
   /**
-   * Get computed style of an element.
+   * A standardise way to obtain an element reference by using an element Id or the object itself.
    * @param {string | Element | Window} targetReference - Receive either a string Id, an element
    *                                                      or window reference.
    * @param {Document} [altDocument=Document] - Document object that is used to resolve
