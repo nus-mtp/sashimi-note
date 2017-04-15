cff2d986a99a01a631a11d5e53c81e24ae2a70e4
tharain <won.jr@hotmail.com>
2017-04-16 03:08:03 +0800

    Add test case for multiple creation to sqlArray.spec.js

--- sashimi-webapp/test/unit/specs/database/generated-data/sqlArray.spec.js
+++ sashimi-webapp/test/unit/specs/database/generated-data/sqlArray.spec.js
@@ -23,5 +23,19 @@ describe('sqlArray', () => {
       expect(typeof alaArray[0]).to.equal('object');
       expect(typeof Object.values(alaArray[0])).to.equal.null;
     });
+
+    it('should fill up multiple objects', () => {
+      alasqlArray.initializeAlasqlArray();
+      alasqlArray.addKeyBasePair('a', 123);
+      alasqlArray.addKeyBasePair('b', '123');
+      alasqlArray.initializeNextAlasqlArray();
+      alasqlArray.addKeyBasePair('c', 456);
+      alasqlArray.addKeyBasePair('d', '789');
+      const alaArray = alasqlArray.endAlasqlArray();
+      expect(alaArray).to.deep.equal([{ a: 123, b: '123' }, { c: 456, d: '789' }]);
+      expect(typeof alaArray).to.equal('object');
+      expect(typeof alaArray[0]).to.equal('object');
+      expect(typeof Object.values(alaArray[0])).to.equal.null;
+    });
   });
 });
