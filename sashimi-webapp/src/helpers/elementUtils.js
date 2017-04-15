f888e0ead3286dd100e42385142e5b57f4024a75
Amos Wong <amoswwj@gmail.com>
2017-04-15 14:37:37 +0800

    Update scrolling code from Pawel Grzybek

--- sashimi-webapp/src/helpers/elementUtils.js
+++ sashimi-webapp/src/helpers/elementUtils.js
@@ -39,48 +39,83 @@ export default {
     return doc.defaultView || doc.parentWindow;
   },
 
-  scrollTo(destinationElement, duration = 200, easing = 'linear', callback) {
+  /**
+   * @param {(number|HTMLElement)} destination - Destination to scroll to (DOM element or number)
+   * @param {number} duration - Duration of scrolling animation
+   * @param {string} easing - Timing function name (Allowed values: 'linear')
+   * @param {function} callback - Optional callback invoked after animation
+   * @author Pawel Grzybek
+   */
+  scrollTo(destination, duration = 200, easing = 'linear', callback) {
+    // Predefine list of available timing functions
+    // If you need more, tween js is full of great examples
+    // https://github.com/tweenjs/tween.js/blob/master/src/Tween.js#L421-L737
     const easings = {
       linear(t) {
         return t;
-      }
+      },
     };
-    const viewerDoc = this.getDocument(destinationElement);
-    const viewWindow = this.getWindow(destinationElement);
 
+    // Handle case where this code is used in an iframe element
+    let viewerDoc = this.getDocument(destination);
+    let viewWindow = this.getWindow(destination);
+    if (typeof destination === 'number') {
+      viewerDoc = document;
+      viewWindow = window;
+    }
+
+    // Store initial position of a window and time
+    // If performance is not available in your browser
+    // It will fallback to new Date().getTime() - thanks IE < 10
     const start = viewWindow.pageYOffset;
-    const startTime = 'now' in viewWindow.performance ? performance.now() : new Date().getTime();
+    const startTime = ('now' in viewWindow.performance) ? performance.now() : new Date().getTime();
 
-    const documentHeight = Math.max(viewerDoc.body.scrollHeight, viewerDoc.body.offsetHeight, viewerDoc.documentElement.clientHeight, viewerDoc.documentElement.scrollHeight, viewerDoc.documentElement.offsetHeight);
-    const windowHeight = viewWindow.innerHeight || viewerDoc.documentElement.clientHeight || viewerDoc.getElementsByTagName('body')[0].clientHeight;
-    const destinationOffset = typeof destinationElement === 'number' ? destinationElement : destinationElement.offsetTop;
-    const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+    // Take height of window and document to resolve max scrollable value
+    // Prevent requestAnimationFrame() from scrolling below maximum scollable value
+    // Resolve destination type (node or number)
+    const documentHeight = Math.max(viewerDoc.body.scrollHeight,
+                                    viewerDoc.body.offsetHeight,
+                                    viewerDoc.documentElement.clientHeight,
+                                    viewerDoc.documentElement.scrollHeight,
+                                    viewerDoc.documentElement.offsetHeight);
+    const windowHeight = viewWindow.innerHeight ||
+                         viewerDoc.documentElement.clientHeight ||
+                         viewerDoc.getElementsByTagName('body')[0].clientHeight;
+    const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+    const destinationOffsetToScroll = Math.round((documentHeight - destinationOffset) < windowHeight
+                                                  ? documentHeight - windowHeight
+                                                  : destinationOffset
+                                                );
 
+    // If requestAnimationFrame is not supported
+    // Move window to destination position and trigger callback function
     if ('requestAnimationFrame' in viewWindow === false) {
-      scrollTo(destinationOffsetToScroll);
-      // viewWindow.scroll(0, destinationOffsetToScroll);
-      if (callback) {
-        callback();
-      }
+      viewWindow.scroll(0, destinationOffsetToScroll);
+      if (callback) { callback(); }
       return;
     }
 
+    // function resolves position of a window and moves to exact amount of pixels
+    // Resolved by calculating delta and timing function choosen by user
     function scroll() {
       const now = 'now' in viewWindow.performance ? performance.now() : new Date().getTime();
       const time = Math.min(1, ((now - startTime) / duration));
       const timeFunction = easings[easing](time);
       viewWindow.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
 
+      // Stop requesting animation when window reached its destination
+      // And run a callback function
       if (viewWindow.pageYOffset === destinationOffsetToScroll) {
-        if (callback) {
-          callback();
-        }
+        if (callback) { callback(); }
         return;
       }
 
+      // If window still needs to scroll to reach destination
+      // Request another scroll invokation
       requestAnimationFrame(scroll);
     }
 
+    // Invoke scroll and sequential requestAnimationFrame
     scroll();
-  }
+  },
 };
