d51987a7c08f98c6453a66e30832d42238ebff84
amoshydra <amoswwj@gmail.com>
2017-04-20 13:06:11 +0800

    Expand line injection code for more markdown rules

--- sashimi-webapp/src/logic/documentPackager/plugins/markdown-it-line-number.js
+++ sashimi-webapp/src/logic/documentPackager/plugins/markdown-it-line-number.js
@@ -1,19 +1,44 @@
-function injectDataLine(tokens, idx, options, env, slf) {
-  if (tokens[idx].map && tokens[idx].level === 0) {
-    const lineStart = tokens[idx].map[0] + 1;
-    const lineEnd = tokens[idx].map[1];
-    tokens[idx].attrJoin('class', 'code-line');
-    tokens[idx].attrSet('data-line-start', `${lineStart}`);
-    tokens[idx].attrSet('data-line-end', `${lineEnd}`);
-  }
-  return slf.renderToken(tokens, idx, options, env, slf);
+
+function injectWrapper(defaultRenderer = null) {
+  const injectDataLine = function injectDataLine(tokens, idx, options, env, slf) {
+    if (defaultRenderer) { // Run default renderer first if avaiable
+      defaultRenderer(tokens, idx, options, env, slf);
+    }
+
+    if (tokens[idx].map && tokens[idx].level < 3) {
+      const lineStart = tokens[idx].map[0] + 1;
+      const lineEnd = tokens[idx].map[1];
+      tokens[idx].attrJoin('class', 'code-line');
+      tokens[idx].attrSet('data-line-start', `${lineStart}`);
+      tokens[idx].attrSet('data-line-end', `${lineEnd}`);
+    }
+
+    return slf.renderToken(tokens, idx, options, env, slf);
+  };
+
+  return injectDataLine;
 }
 
 function dataLineInjector(md, options) {
   // Minimal line tracking by injecting only
   // the paragraph and header tag
-  md.renderer.rules.paragraph_open = injectDataLine;
-  md.renderer.rules.heading_open = injectDataLine;
+  const injectionNames = [
+    'paragraph_open',
+    'heading_open',
+    'blockquote_open',
+    'table_open',
+    'bullet_list_open',
+    'list_item_open',
+    'ordered_list_open',
+    // 'container_classname_open', // not needed as it contained mostly markdown data
+    'container_spoiler_open',
+  ];
+
+  injectionNames.forEach((name) => {
+    const rule = md.renderer.rules[name];
+    md.renderer.rules[name] = injectWrapper(rule);
+  });
+  console.log(md.renderer.rules);
 }
 
 export default dataLineInjector;
